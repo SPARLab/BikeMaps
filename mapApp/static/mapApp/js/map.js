10df0de4f00609ec4db25a1f8c1aca6fea95a273
Taylor Denouden <tayden123@hotmail.com>
2014-05-28 13:58:17 -0700

    Added a bunch of tile layers for comparisons

--- mapApp/static/mapApp/js/map.js
+++ mapApp/static/mapApp/js/map.js
@@ -7,25 +7,109 @@ var map;
 /* Create the map with a tile layer and set global variable map */
 function initialize(){
 
-	/* OSM Cycle Map basemap tile layer */
-	mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
-	ocmlink = '<a href="http://thunderforest.com/">Thunderforest</a>';
-	var OCM = L.tileLayer(
-	    'http://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png', {
-	    attribution: '&copy; '+mapLink+' Contributors & '+ocmlink,
+
+	/* Used <http://josm.openstreetmap.de/wiki/Maps> for map tiles */
+
+/* BASEMAPS */
+	/* We don't need all of these. Just for visualization so we can pick a few */
+	/* OSM Cycle Map basemap tile layer */	
+	var openCycleMap = L.tileLayer(
+	    'http://tile.opencyclemap.org/cycle/{z}/{x}/{y}.png', {
+	    attribution: '&copy <a href=http://openstreetmap.org>OpenStreetMap</a> contributors, CC-BY-SA',
 	    maxZoom: 18,
 	    });
+	
 
-	/* Mapbox basemap tile layer */
-	var mapBox = L.tileLayer('http://{s}.tiles.mapbox.com/v3/tayden.ibi2aoib/{z}/{x}/{y}.png', {
-	    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> \
-	    	contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, \
-	    	Imagery © <a href="http://mapbox.com">Mapbox</a>',
+	var mapbox = L.tileLayer('http://{s}.tiles.mapbox.com/v3/tayden.ibi2aoib/{z}/{x}/{y}.png', {
+	    attribution: 'Tiles Courtesy of <a href=https://www.mapbox.com/>Mapboxt</a>, \
+	    	map data &copy <a href=http://openstreetmap.org>OpenStreetMap</a> contributors',
 	    maxZoom: 18
 	});
+
+    var humanitarianOSM = L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+	    attribution: '&copy <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, tiles courtesy of <a href=http://hot.openstreetmap.org/>Humanitarian OpenStreetMap Team</a>',
+	    maxZoom: 20
+	});
+
+    var mapboxSat = L.tileLayer('http://{s}.tiles.mapbox.com/v3/openstreetmap.map-4wvf9l0l/{z}/{x}/{y}.png', {
+    	attribution: 'Tiles Courtesy of <a href=https://www.mapbox.com/>Mapboxt</a>, \
+	    	map data &copy <a href=http://openstreetmap.org>OpenStreetMap</a> contributors',
+	    maxZoom: 18
+	});
+
+    var mapQuest = L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
+	    attribution: 'Tiles Courtesy of <a href=https://open.mapquest.com/>MapQuest</a>, \
+	    	map data &copy <a href=http://openstreetmap.org>OpenStreetMap</a> contributors',
+	    maxZoom: 18,
+	    subdomains: '1234' // Switch between subdomains {s} 1,2,3,4 instead of a,b,c
+	});
+
+
+	var osmMapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	    attribution: '&copy <a href=https://openstreetmap.org/>OpenStreetMap</a> contributors, CC-BY-SA',
+	    maxZoom: 19,
+	});
+
+
+	var osmMapnikBW = L.tileLayer('http://{s}.www.toolserver.org/tiles/bw-mapnik/{z}/{x}/{y}.png', {
+	    attribution: '&copy <a href=https://openstreetmap.org/>OpenStreetMap</a> contributors, CC-BY-SA',
+	    maxZoom: 18,
+	});
+
+
+	/* Define which map tiles are basemaps */
+	var baseMaps = {
+		"Open Street Map": osmMapnik,
+		"Open Street Map B&W": osmMapnikBW,
+		"Open Cycle Map": openCycleMap,
+		"Humanitarian OSM": humanitarianOSM,
+		"Mapbox": mapbox,
+		"Mapbox Satellite": mapboxSat,
+		"MapQuest OSM": mapQuest,
+
+	};
+
+
+
+/* OVERLAY MAPS */
 	
 	/* OSM Strava heatmap tile layer */
-	var stravaHM = L.tileLayer('http://gometry.strava.com/tiles/cycling/color1/{z}/{x}/{y}.png', {
+	var stravaHM1 = L.tileLayer('http://gometry.strava.com/tiles/cycling/color1/{z}/{x}/{y}.png', {
+	    attribution: 'Heatmap &copy <a href=http://labs.strava.com/heatmap/>Strava labs</a>',
+	    minZoom: 3,
+	    maxZoom: 17,
+	    opacity: 0.5
+	});
+
+	var stravaHM2 = L.tileLayer('http://gometry.strava.com/tiles/cycling/color2/{z}/{x}/{y}.png', {
+	    attribution: 'Heatmap &copy <a href=http://labs.strava.com/heatmap/>Strava labs</a>',
+	    minZoom: 3,
+	    maxZoom: 17,
+	    opacity: 0.5
+	});
+
+	var stravaHM3 = L.tileLayer('http://gometry.strava.com/tiles/cycling/color3/{z}/{x}/{y}.png', {
+	    attribution: 'Heatmap &copy <a href=http://labs.strava.com/heatmap/>Strava labs</a>',
+	    minZoom: 3,
+	    maxZoom: 17,
+	    opacity: 0.5
+	});
+
+	var stravaHM4 = L.tileLayer('http://gometry.strava.com/tiles/cycling/color4/{z}/{x}/{y}.png', {
+	    attribution: 'Heatmap &copy <a href=http://labs.strava.com/heatmap/>Strava labs</a>',
+	    minZoom: 3,
+	    maxZoom: 17,
+	    opacity: 0.5
+	});
+
+	var stravaHM5 = L.tileLayer('http://gometry.strava.com/tiles/cycling/color5/{z}/{x}/{y}.png', {
+	    attribution: 'Heatmap &copy <a href=http://labs.strava.com/heatmap/>Strava labs</a>',
+	    minZoom: 3,
+	    maxZoom: 17,
+	    opacity: 0.5
+	});
+
+	var stravaHM6 = L.tileLayer('http://gometry.strava.com/tiles/cycling/color6/{z}/{x}/{y}.png', {
 	    attribution: 'Heatmap &copy <a href=http://labs.strava.com/heatmap/>Strava labs</a>',
 	    minZoom: 3,
 	    maxZoom: 17,
@@ -33,22 +117,21 @@ function initialize(){
 	});
 
 
-	/* Define which map tiles are basemaps */
-	var baseMaps = {
-		"MapBox": mapBox,
-		"Open Cycle Map": OCM
-	};
-
 	/* Define which map tiles are overlays */
 	var overlayMaps = {
-		"Strava heatmap": stravaHM
-	};
-	
+		"Strava heatmap 1": stravaHM1,
+		"Strava heatmap 2": stravaHM2,
+		"Strava heatmap 3": stravaHM3,
+		"Strava heatmap 4": stravaHM4,
+		"Strava heatmap 5": stravaHM5,
+		"Strava heatmap 6": stravaHM6,
+	}
+/* DEFAULTS AND PANEL */	
 	/* Set map center, zoom, and default layers */
 	map = L.map('map', {
 		center: [48.455, -123.3],
 		zoom: 13,
-		layers: [mapBox, stravaHM]
+		layers: [humanitarianOSM, stravaHM5]
 	});
 	
 	/* Create the control panel and render the map */
