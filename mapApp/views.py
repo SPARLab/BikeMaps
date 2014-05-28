c123c6081babe7296de330256e7e124551b0a83e
Taylor Denouden <tayden123@hotmail.com>
2014-05-28 16:36:01 -0700

    Created datetime picker

--- mapApp/views.py
+++ mapApp/views.py
@@ -54,29 +54,3 @@ def sponsors(request):
 def contact(request):
 	return render(request, 'mapApp/contact.html')
 
-# def detail(request, poll_id):
-#     poll = get_object_or_404(Poll, pk=poll_id)
-#     return render(request, 'polls/detail.html', {'poll': poll})
-
-# def results(request, poll_id):
-#     poll = get_object_or_404(Poll, pk=poll_id)
-#     return render(request, 'polls/results.html', {'poll': poll})
-
-# def vote(request, poll_id):
-#     p = get_object_or_404(Poll, pk=poll_id)
-#     try:
-#         selected_choice = p.choice_set.get(pk=request.POST['choice'])
-#     except (KeyError, Choice.DoesNotExist):
-#         # Redisplay the poll voting form.
-#         return render(request, 'polls/detail.html', {
-#             'poll': p,
-#             'error_message': "You didn't select a choice.",
-#         })
-#     else:
-#         selected_choice.votes += 1
-#         selected_choice.save()
-#         # Always return an HttpResponseRedirect after successfully dealing
-#         # with POST data. This prevents data from being posted twice if a
-#         # user hits the Back button.
-#         return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
-
