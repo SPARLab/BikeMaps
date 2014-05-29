3331afff7ad7788fde189664fd030ecb4123ec1f
Taylor Denouden <tayden123@hotmail.com>
2014-05-28 18:04:41 -0700

    Set date pick restriction on date picker

--- mapApp/static/bootstrap/js/datepicker/bootstrap-datetimepicker.js
+++ mapApp/static/bootstrap/js/datepicker/bootstrap-datetimepicker.js
@@ -171,7 +171,7 @@
 					.toggleClass('glyphicon-arrow-left glyphicon-arrow-right');
 			} else {
 				this.picker.find('.prev i, .next i')
-					.toggleClass('icon-arrow-left icon-arrow-right');
+					.toggleClass('glyphicon-arrow-left glyphicon-arrow-right');
 			}
 			;
 
@@ -1608,9 +1608,9 @@
 		},
 		headTemplate:     '<thead>' +
 							  '<tr>' +
-							  '<th class="prev"><i class="icon-arrow-left"/></th>' +
+							  '<th class="prev"><i class="glyphicon glyphicon-arrow-left"/></th>' +
 							  '<th colspan="5" class="switch"></th>' +
-							  '<th class="next"><i class="icon-arrow-right"/></th>' +
+							  '<th class="next"><i class="glyphicon glyphicon-arrow-right"/></th>' +
 							  '</tr>' +
 			'</thead>',
 		headTemplateV3:   '<thead>' +
