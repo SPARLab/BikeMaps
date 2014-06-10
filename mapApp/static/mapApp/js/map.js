3b1eff6747633d4d0b4139725622bc6580871e2a
Taylor Denouden <tayden123@hotmail.com>
2014-06-10 14:12:55 -0700

    Changed bike lane colours and reindented js files

--- mapApp/static/mapApp/js/map.js
+++ mapApp/static/mapApp/js/map.js
@@ -6,71 +6,137 @@
 var map;
 
 // Dynamically clustered point data layer
-var accidentPoints = new L.MarkerClusterGroup({maxClusterRadius: 50})  //, disableClusteringAtZoom: 16});
+var accidentPoints = new L.MarkerClusterGroup({
+		maxClusterRadius: 50
+	}) //, disableClusteringAtZoom: 16});
 
 // Heatmap layer corresponding to all accident data
-var heatMap = L.heatLayer([], {radius: 50, blur:20, opacity: 1});
+var heatMap = L.heatLayer([], {
+	radius: 50,
+	blur: 20,
+	opacity: 1
+});
 
 // Custom icons (Using Maki icon symbols)
-var bikeRedIcon = L.MakiMarkers.icon({icon: "bicycle", color:"#d9534f", size: "m"});
-var bikeYellowIcon = L.MakiMarkers.icon({icon: "bicycle", color:"#f0ad4e", size: "m"});
-var policeIcon = L.MakiMarkers.icon({icon: "police", color:"#428bca", size: "m"});
+var bikeRedIcon = L.MakiMarkers.icon({
+	icon: "bicycle",
+	color: "#d9534f",
+	size: "m"
+});
+var bikeYellowIcon = L.MakiMarkers.icon({
+	icon: "bicycle",
+	color: "#f0ad4e",
+	size: "m"
+});
+var policeIcon = L.MakiMarkers.icon({
+	icon: "police",
+	color: "#428bca",
+	size: "m"
+});
 
 
-var poPo = new L.geoJson(policeData, {
-                pointToLayer: function (feature, latlng){
-                    return L.marker(latlng, {icon: policeIcon});
-                },
-                onEachFeature: function(feature, layer) {
-                    layer.bindPopup(feature.properties.ON_STREET);
-                }
-            });
-            accidentPoints.addLayer(poPo);
+var policePoints = new L.geoJson(policeData, {
+	pointToLayer: function(feature, latlng) {
+		return L.marker(latlng, {
+			icon: policeIcon
+		});
+	},
+	onEachFeature: function(feature, layer) {
+		layer.bindPopup('<strong>' + feature.properties.ACC_DATE + '</strong><br>' + feature.properties.ACC_TYPE);
+	}
+});
 
-            
+accidentPoints.addLayer(policePoints);
+
+var bikeLanes = new L.geoJson(bikeRoutes, {
+	style: function(feature) {
+		switch (feature.properties.Descriptio) {
+			case 'Buffered Bike Lane':
+				return {
+					color: '#007f16', // Dark green
+				};
+			case 'Cycle Track*':
+				return {
+					color: '#259238', // Faded dark green
+					opacity: 0.8
+				};
+			case 'Multi-Use Trail':
+				return {
+					color: '#87a000', // Dark Yellow
+					opacity: 0.8
+				};
+			case 'Conventional Bike Lane':
+				return {
+					color: '#00c322', // Green
+					opacity: 0.8
+				};
+			case 'Priority Transit and Cycling Lanes*':
+				return {
+					color: '#05326d', // Dark blue
+					opacity: 0.8
+				};
+			case 'Signed Bike Route':
+				return {
+					color: '#0e51a7', // blue
+					opacity: 0.8
+				};
+			case 'Proposed Bicycle Network':
+				return {
+					color: '#ff9e00', // Faded green
+					opacity: 0.3
+				};
+		}
+	}
+});
+
+
+
+
+
+
 /* Create the map with a tile layer and set global variable map */
-function initialize(){
-/* BASEMAPS */
+function initialize() {
+	/* BASEMAPS */
 	var openCycleMap = L.tileLayer(
-	    'http://tile.opencyclemap.org/cycle/{z}/{x}/{y}.png', {
-	    attribution: '&copy <a href=http://openstreetmap.org>OpenStreetMap</a> contributors, CC-BY-SA',
-	    maxZoom: 18,
-	    });
-	
+		'http://tile.opencyclemap.org/cycle/{z}/{x}/{y}.png', {
+			attribution: '&copy <a href=http://openstreetmap.org>OpenStreetMap</a> contributors, CC-BY-SA',
+			maxZoom: 18,
+		});
+
 	var mapbox = L.tileLayer('http://{s}.tiles.mapbox.com/v3/tayden.ibi2aoib/{z}/{x}/{y}.png', {
-	    attribution: 'Tiles courtesy of <a href=https://www.mapbox.com/>Mapbox</a>, \
+		attribution: 'Tiles courtesy of <a href=https://www.mapbox.com/>Mapbox</a>, \
 	    	map data &copy <a href=http://openstreetmap.org>OpenStreetMap</a> contributors',
-	    maxZoom: 18
+		maxZoom: 18
 	});
 
-    var humanitarianOSM = L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-	    attribution: '&copy <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, tiles courtesy of <a href=http://hot.openstreetmap.org/>Humanitarian OpenStreetMap Team</a>',
-	    maxZoom: 20
+	var humanitarianOSM = L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+		attribution: '&copy <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, tiles courtesy of <a href=http://hot.openstreetmap.org/>Humanitarian OpenStreetMap Team</a>',
+		maxZoom: 20
 	});
 
-    var mapboxSat = L.tileLayer('http://{s}.tiles.mapbox.com/v3/openstreetmap.map-4wvf9l0l/{z}/{x}/{y}.png', {
-    	attribution: 'Tiles courtesy of <a href=https://www.mapbox.com/>Mapbox</a>, \
+	var mapboxSat = L.tileLayer('http://{s}.tiles.mapbox.com/v3/openstreetmap.map-4wvf9l0l/{z}/{x}/{y}.png', {
+		attribution: 'Tiles courtesy of <a href=https://www.mapbox.com/>Mapbox</a>, \
 	    	map data &copy <a href=http://openstreetmap.org>OpenStreetMap</a> contributors',
-	    maxZoom: 18
+		maxZoom: 18
 	});
 
-    var mapQuest = L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
-	    attribution: 'Tiles courtesy of <a href=https://open.mapquest.com/>MapQuest</a>, \
+	var mapQuest = L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
+		attribution: 'Tiles courtesy of <a href=https://open.mapquest.com/>MapQuest</a>, \
 	    	map data &copy <a href=http://openstreetmap.org>OpenStreetMap</a> contributors',
-	    maxZoom: 18,
-	    subdomains: '1234' // Switch between subdomains {s} 1,2,3,4 instead of a,b,c
+		maxZoom: 18,
+		subdomains: '1234' // Switch between subdomains {s} 1,2,3,4 instead of a,b,c
 	});
 
 
 	var osmMapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	    attribution: '&copy <a href=https://openstreetmap.org/>OpenStreetMap</a> contributors, CC-BY-SA',
-	    maxZoom: 19,
+		attribution: '&copy <a href=https://openstreetmap.org/>OpenStreetMap</a> contributors, CC-BY-SA',
+		maxZoom: 19,
 	});
 
 
 	var osmMapnikBW = L.tileLayer('http://{s}.www.toolserver.org/tiles/bw-mapnik/{z}/{x}/{y}.png', {
-	    attribution: '&copy <a href=https://openstreetmap.org/>OpenStreetMap</a> contributors, CC-BY-SA',
-	    maxZoom: 18,
+		attribution: '&copy <a href=https://openstreetmap.org/>OpenStreetMap</a> contributors, CC-BY-SA',
+		maxZoom: 18,
 	});
 
 
@@ -87,13 +153,13 @@ function initialize(){
 
 
 
-/* OVERLAY MAPS */
+	/* OVERLAY MAPS */
 	/* OSM Strava heatmap tile layer */
 	var stravaHM5 = L.tileLayer('http://gometry.strava.com/tiles/cycling/color5/{z}/{x}/{y}.png', {
-	    attribution: 'ridership data &copy <a href=http://labs.strava.com/heatmap/>Strava labs</a>',
-	    minZoom: 3,
-	    maxZoom: 17,
-	    opacity: 0.5
+		attribution: 'ridership data &copy <a href=http://labs.strava.com/heatmap/>Strava labs</a>',
+		minZoom: 3,
+		maxZoom: 17,
+		opacity: 0.5
 	});
 
 
@@ -102,18 +168,20 @@ function initialize(){
 		"Accident heat map": heatMap,
 		"Accident points": accidentPoints,
 		"Ridership heat map": stravaHM5,
+		"Bike lanes": bikeLanes,
 	}
 
-/* DEFAULTS AND PANEL */	
+	/* DEFAULTS AND PANEL */
 	/* Set map center, zoom, default layers and render */
 	map = L.map('map', {
 		center: [48.5, -123.3],
 		zoom: 11,
-		layers: [mapbox, accidentPoints, stravaHM5] /* Layers to display on load */
+		layers: [mapbox, accidentPoints, bikeLanes] /* Layers to display on load */
 	});
-	
+
 	/* Create the control panel */
 	L.control.layers(baseMaps, overlayMaps).addTo(map);
+
 
 }
 
@@ -122,35 +190,37 @@ function initialize(){
 
 /* Create a popup when map is clicked with button to add a new point  */
 function mapClickPrompt(e) {
-	var popup = L.popup({'closeOnClick': true, });
-    popup
-        .setLatLng(e.latlng)
-        .setContent('<button class="btn btn-primary btn-lg" data-toggle="modal" \
+	var popup = L.popup({
+		'closeOnClick': true,
+	});
+	popup
+		.setLatLng(e.latlng)
+		.setContent('<button class="btn btn-primary btn-lg" data-toggle="modal" \
         	data-target="#incidentForm"><span class="glyphicon glyphicon-pushpin"></span></button>')
-        .openOn(map);
+		.openOn(map);
 
-    //Set point field in form to click location
-    document.getElementById("point").value = ('Point('+e.latlng.lng+' '+e.latlng.lat+')');
+	//Set point field in form to click location
+	document.getElementById("point").value = ('Point(' + e.latlng.lng + ' ' + e.latlng.lat + ')');
 }
 
 
 function addPoint(latlng, msg, type) {
-    heatMap.addLatLng(latlng);
+	heatMap.addLatLng(latlng);
 
-    var icon;
-    if (type == "police") {
-    	icon = policeIcon;
-    }
-    else if (type == "Collision") {
-    	icon = bikeRedIcon;
-    }
-    else {
-    	icon = bikeYellowIcon;
-    }
-	marker = L.marker(latlng, {icon: icon});
-    marker.bindPopup(msg);
-    
-    accidentPoints.addLayer(marker);
+	var icon;
+	if (type == "police") {
+		icon = policeIcon;
+	} else if (type == "Collision") {
+		icon = bikeRedIcon;
+	} else {
+		icon = bikeYellowIcon;
+	}
+	marker = L.marker(latlng, {
+		icon: icon
+	});
+	marker.bindPopup(msg);
+
+	accidentPoints.addLayer(marker);
 }
 
 function toggleICBC() {
@@ -162,5 +232,7 @@ function toggleBikeRacks() {
 }
 
 function locateUser() {
-    this.map.locate({setView : true});
+	this.map.locate({
+		setView: true
+	});
 }
