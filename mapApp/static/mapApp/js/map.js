4255a5e0d6cccacbf4589f9244ba78b1c8da9a54
Taylor Denouden <tayden123@hotmail.com>
2014-06-20 16:48:00 -0700

    roughed in the routing plugin

--- mapApp/static/mapApp/js/map.js
+++ mapApp/static/mapApp/js/map.js
@@ -142,6 +142,75 @@ function initialize() {
 		edit: false,
 	}));
 
+	/* LEAFLET-ROUTING */
+/*
+	// Snapping Layer
+    snapping = new L.geoJson(null, {
+      style: {
+        opacity:0
+        ,clickable:false
+      }
+    }).addTo(map);
+    map.on('moveend', function() {
+      if (map.getZoom() > 12) {
+        var proxy = 'http://www2.turistforeningen.no/routing.php?url=';
+        var route = 'http://www.openstreetmap.org/api/0.6/map';
+        var params = '&bbox=' + map.getBounds().toBBoxString() + '&1=2';
+        $.get(proxy + route + params).always(function(osm, status) {
+          if (status === 'success' && typeof osm === 'object') {
+            var geojson = osmtogeojson(osm);
+
+            snapping.clearLayers();
+            for (var i = 0; i < geojson.features.length; i++) {
+              var feat = geojson.features[i];
+              if (feat.geometry.type === 'LineString' && feat.properties.tags.highway) {
+                snapping.addData(geojson.features[i]);
+              }
+            }
+          } else {
+            console.log('Could not load snapping data');
+          }
+        });
+      } else {
+        snapping.clearLayers();
+      }
+    });
+    map.fire('moveend');
+
+    // OSM Router
+    router = function(m1, m2, cb) {
+      var proxy = 'http://www2.turistforeningen.no/routing.php?url=';
+      var route = 'http://www.yournavigation.org/api/1.0/gosmore.php&format=geojson&v=foot&fast=1&layer=mapnik';
+      var params = '&flat=' + m1.lat + '&flon=' + m1.lng + '&tlat=' + m2.lat + '&tlon=' + m2.lng;
+      $.getJSON(proxy + route + params, function(geojson, status) {
+        if (!geojson || !geojson.coordinates || geojson.coordinates.length === 0) {
+          if (typeof console.log === 'function') {
+            console.log('OSM router failed', geojson);
+          }
+          return cb(new Error());
+        }
+        return cb(null, L.GeoJSON.geometryToLayer(geojson));
+      });
+    }
+
+    routing = new L.Routing({
+      position: 'topleft'
+      ,routing: {
+        router: router
+      }
+      ,snapping: {
+        layers: []
+      }
+      ,snapping: {
+        layers: [snapping]
+        ,sensitivity: 15
+        ,vertexonly: false
+      }
+    });
+    map.addControl(routing);
+    routing.draw()
+*/
+
 }
 
 function initializeGeoJsonLayers(){
@@ -447,7 +516,7 @@ function pieChart(data, outerR, innerR, total){
 	});
 };
 
-// Purpose: Helper function to convert xml markup into html code (...I think)
+// Purpose: Helper function to convert xmlNode into a string
 function serializeXmlNode(xmlNode) {
     if (typeof window.XMLSerializer != "undefined") {
         return (new window.XMLSerializer()).serializeToString(xmlNode);
