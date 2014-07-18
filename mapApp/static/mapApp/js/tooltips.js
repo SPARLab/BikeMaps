1b8b44ba41e2fe252fa72a5d63d5344ebd59f81f
Taylor Denouden <tayden123@hotmail.com>
2014-07-18 11:19:03 -0700

    fixed bug where help button tooltip would flash

--- mapApp/static/mapApp/js/tooltips.js
+++ mapApp/static/mapApp/js/tooltips.js
@@ -2,17 +2,19 @@
 
 // Tool tip definitions. Uses title attribut of html tag.
  $(document).ready(function(){
+ 	// layer control
+	$('.leaflet-control-layers-toggle').tipsy({gravity:'e', trigger: 'manual', title: function(){ return "Layer control"}, fade: true});
+	// search control
+	$('.leaflet-control-geocoder').tipsy({gravity:'e', delayIn:400, fade: true, title: function(){ return "Search for an address"}});
+ 	// delete points control
  	$('.leaflet-right .leaflet-draw-edit-remove').prop('title', 'Delete user points');
-
+ 	$('.leaflet-right .leaflet-draw-edit-remove').tipsy({gravity:'e', delayIn: 400, fade: true});
+	// help button
 	$('.leaflet-bar-part').tipsy({gravity:'e', delayIn: 200, fade: true});
-	
-	$('.leaflet-right .leaflet-control a').tipsy({gravity:'e', delayIn: 400, fade: true});
+
+	// All left side controls
 	$('.leaflet-left .leaflet-control a').tipsy({gravity:'w', delayIn: 400, fade: true});
 	
-	$('.leaflet-control-geocoder').tipsy({gravity:'e', delayIn:400, fade: true, title: function(){ return "Search for an address"}});
-	$('.leaflet-control-layers-toggle').tipsy({gravity:'e', trigger: 'manual', title: function(){ return "Layer control"}, fade: true});
-	// $('.leaflet-right .leaflet-draw-edit-remove').tipsy({gravity:'e', delayIn:400, fade: true, title: function(){ return "Delete user points"}});
-
 	$('#map').click(function(){toggleTooltips("hide")});
 });
 
