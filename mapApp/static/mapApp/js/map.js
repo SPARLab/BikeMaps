d1db88f2c9306191ef8ec2b2de7f553cff4d96f5
Taylor Denouden <tayden123@hotmail.com>
2014-07-23 12:22:21 -0700

    revamped setView logic and did major cleanup of map.js

--- mapApp/static/mapApp/js/map.js
+++ mapApp/static/mapApp/js/map.js
@@ -82,32 +82,26 @@ var	accidentPoints = new L.MarkerClusterGroup({
 			return new L.DivIcon({ className: 'rack-cluster' + c, iconSize: size});
 		},
 	}),
+
+	// Tile layers
+	skobbler = L.tileLayer('http://tiles1-b586b1453a9d82677351c34485e59108.skobblermaps.com/TileService/tiles/2.0/1111113120/10/{z}/{x}/{y}.png@2x', 
+		{minZoom: 2, attribution: '© Tiles: <a href="http://maps.skobbler.com/">skobbler</a>, Map data: <a href=http://openstreetmap.org>OpenStreetMap</a> contributors, CC-BY-SA'}),
 	
-	layerControl = L.control.layers();
+	skobblerNight = L.tileLayer('http://tiles1-b586b1453a9d82677351c34485e59108.skobblermaps.com/TileService/tiles/2.0/1111113120/2/{z}/{x}/{y}.png@2x', 
+		{minZoom: 2, attribution: '© Tiles: <a href="http://maps.skobbler.com/">skobbler</a>, Map data: <a href=http://openstreetmap.org>OpenStreetMap</a> contributors, CC-BY-SA'}),
+
+	stravaHM = L.tileLayer('http://gometry.strava.com/tiles/cycling/color5/{z}/{x}/{y}.png', 
+		{minZoom: 3, maxZoom: 17, opacity: 0.8, attribution: 'Ridership data &copy <a href=http://labs.strava.com/heatmap/>Strava labs</a>'});
 
 
 /* Create the map with a tile layer and set global variable map */
-function initialize(lat, lng, zoom) {
-	/* STATIC VECTOR DEFINITIONS */
+function initialize(mobile) {
+	mobile = typeof mobile !== 'undefined' ? mobile : false; //default value of mobile is false
+
+	// Static vector definitions 
 	initializeGeoJsonLayers();
 
-	/* TILE LAYER DEFINITIONS */
-	var	skobblerUrl = 'http://tiles1-b586b1453a9d82677351c34485e59108.skobblermaps.com/TileService/tiles/2.0/1111113120/10/{z}/{x}/{y}.png@2x',
-		skobblerAttrib = '© Tiles: <a href="http://maps.skobbler.com/">skobbler</a>, Map data: <a href=http://openstreetmap.org>OpenStreetMap</a> contributors, CC-BY-SA',
-		skobbler = L.tileLayer(skobblerUrl, {minZoom: 2, attribution: skobblerAttrib});
-	layerControl.addBaseLayer(skobbler, 'Day');
-
-	var skobblerNightUrl = 'http://tiles1-b586b1453a9d82677351c34485e59108.skobblermaps.com/TileService/tiles/2.0/1111113120/2/{z}/{x}/{y}.png@2x',
-		skobblerNightAttrib = '© Tiles: <a href="http://maps.skobbler.com/">skobbler</a>, Map data: <a href=http://openstreetmap.org>OpenStreetMap</a> contributors, CC-BY-SA',
-		skobblerNight = L.tileLayer(skobblerNightUrl, {minZoom: 2, attribution: skobblerNightAttrib});
-	layerControl.addBaseLayer(skobblerNight, 'Night');
-
-	var stravaUrl = 'http://gometry.strava.com/tiles/cycling/color5/{z}/{x}/{y}.png',
-		stravaAttrib = 'Ridership data &copy <a href=http://labs.strava.com/heatmap/>Strava labs</a>',
-		stravaHM = L.tileLayer(stravaUrl, {minZoom: 3, maxZoom: 17, opacity: 0.8, attribution: stravaAttrib});
-	layerControl.addOverlay(stravaHM, 'Cyclist density heatmap');
-
-	/* MAP INIT AND DEFAULT LAYERS */
+	// Map init and default layers 
 	map = L.map('map', {
 		center: [48, -100],
 		zoom: 4,
@@ -115,158 +109,179 @@ function initialize(lat, lng, zoom) {
 	});
 	
 	// Add all controls to the map
-	addControls();
-
-	if(zoom) this.map.setView(L.latLng(lat,lng), zoom);
-	else locateUser();
-
-	map.on('locationerror', onLocationError);
-	map.on('locationfound', onLocationFound);
-};
-
-/* FIND AND RETURN THE USER'S LOCATION */
-function locateUser() {
-	this.map.locate({
-		setView: true,
-		maxZoom: 16,
-		// watch: true,
-		enableHighAccuracy: true
-	});
-};
-
-function onLocationError(e) {
-	alert(e.message);
-};
-
-function onLocationFound(e) {
-	// console.log('location found');
-    // if(locationGroup) layerControl.removeLayer(locationGroup);
-    var radius = e.accuracy / 2,
-
-	    marker = L.marker(e.latlng, {icon: locationIcon})
-	        .bindPopup("You are within " + radius + " meters of this point"),
-	    circle = L.circle(e.latlng, radius, {
-	      	color: "#" + locationIcon.options.color,
-			weight: 1,
-			opacity: 0.3,
-			clickable: false,
-			fillOpacity: 0.1
-	    });
-
-    locationGroup = L.layerGroup([marker, circle]);
-    layerControl.addOverlay(locationGroup, 'Detected location<br>' + 
-		'<img class="legend-marker" src="https://api.tiles.mapbox.com/v3/marker/pin-s' 
-			+ '-' + locationIcon.options.icon + '+' + locationIcon.options.color + '.png"> <small>You are here</small>');
-    locationGroup.addTo(map);
-};
-
-function addControls(){
-	/* LAYER CONTROL */
-	layerControl.addOverlay(accidentPoints, 
-		'Incident points<br>' + 
-		'<img class="legend-marker" src="https://api.tiles.mapbox.com/v3/marker/pin-s' 
-			+ '-' + bikeRedIcon.options.icon + '+' + bikeRedIcon.options.color + '.png"> <small>User collision</small><br>' + 
-		'<img class="legend-marker" src="https://api.tiles.mapbox.com/v3/marker/pin-s' 
-		+ '-' + bikeYellowIcon.options.icon + '+' + bikeYellowIcon.options.color + '.png"> <small>User near miss</small><br>' +
-		'<img class="legend-marker" src="https://api.tiles.mapbox.com/v3/marker/pin-s' 
-		+ '-' + policeIcon.options.icon + '+' + policeIcon.options.color + '.png"> <small>Police reported cyclist incident</small><br>' +
-		'<img class="legend-marker" src="https://api.tiles.mapbox.com/v3/marker/pin-s' 
-		+ '-' + icbcIcon.options.icon + '+' + icbcIcon.options.color + '.png"> <small>Cyclist incident insurance claim location</small>'
-
-		);
-	if(!DISABLE_GEOFENCES){
-		layerControl.addOverlay(alertAreas, "Alert Areas");
-	}
-	layerControl.addOverlay(racksCluster, "Bike Racks");
-	layerControl.addOverlay(heatMap, "Accident heat map");
-
-	layerControl.addTo(map);
-
-
-	/* GEOCODING SEARCH BAR CONTROL */
-	var geocoder = L.Control.geocoder({
-		position: "topright"
-	}).addTo(map);
+	addControls(mobile);
 	
-	var geocodeMarker;
-	geocoder.markGeocode = function(result){
-		map.fitBounds(result.bbox);
 
-		if (geocodeMarker) {
-			map.removeLayer(geocodeMarker);
-		}
+	/* GET GEOJSON STATIC LAYERS AND STORE AS LEAFLET FEATURE */
+	function initializeGeoJsonLayers(){
+		var policePoints = new L.geoJson(policeData, {
+			pointToLayer: function(feature, latlng) {
+				heatMap.addLatLng(latlng);
 
-		geocodeMarker = new L.Marker(result.center, {
-			icon: geocodeIcon
-		})
-			.bindPopup(result.name)
-			.addTo(map)
-			.openPopup();
+				return L.marker(latlng, {
+					icon: policeIcon
+				});
+			},
+			onEachFeature: function(feature, layer) {
+				var date = feature.properties.ACC_DATE.split("/");
+				date = getMonthFromInt(parseInt(date[1])) + ' ' + date[2] + ', ' + date[0]; 	// Month dd, YYYY
+				layer.bindPopup('<strong>Source:</strong> Victoria Police Dept.<br><strong>Date:</strong> ' + date);
+			}
+		}).addTo(accidentPoints),
+
+		icbcPoints = new L.geoJson(icbcData, {
+			pointToLayer: function(feature, latlng) {
+				heatMap.addLatLng(latlng);
+
+				return L.marker(latlng, {
+					icon: icbcIcon
+				});
+			},
+			onEachFeature: function(feature, layer) {
+				var date = toTitleCase(feature.properties.Month) + " " + feature.properties.Year;
+				layer.bindPopup('<strong>Source:</strong> ICBC<br><strong>Date: </strong>' + date);
+			}
+		}).addTo(accidentPoints),
+
+		bikeRacksVictoria = new L.geoJson(bikeRacks, {
+			pointToLayer: function(feature, latlng) {
+				return L.marker(latlng);
+			},
+			onEachFeature: function(feature, layer) {
+				layer.bindPopup('Bike rack');
+			}
+		}).addTo(racksCluster);
 	};
 	
+	/* ADD ALL CONTROLS TO THE MAP */
+	function addControls(mobile){
+		/* LAYER CONTROL */
+		layerControl = L.control.layers([],[],{collapsed: mobile});
 
-	/* ADD SCALE BAR */
-	L.control.scale({
-		position: 'bottomright'
-	}).addTo(map);
+		// Add layers
+		layerControl.addBaseLayer(skobbler, 'Day');
+		layerControl.addBaseLayer(skobblerNight, 'Night');
+		layerControl.addOverlay(stravaHM, 'Cyclist density heatmap');
+		layerControl.addOverlay(accidentPoints, 
+			'Incident points<br>' + 
+			'<img class="legend-marker" src="https://api.tiles.mapbox.com/v3/marker/pin-s' 
+				+ '-' + bikeRedIcon.options.icon + '+' + bikeRedIcon.options.color + '.png"> <small>User collision</small><br>' + 
+			'<img class="legend-marker" src="https://api.tiles.mapbox.com/v3/marker/pin-s' 
+			+ '-' + bikeYellowIcon.options.icon + '+' + bikeYellowIcon.options.color + '.png"> <small>User near miss</small><br>' +
+			'<img class="legend-marker" src="https://api.tiles.mapbox.com/v3/marker/pin-s' 
+			+ '-' + policeIcon.options.icon + '+' + policeIcon.options.color + '.png"> <small>Police reported cyclist incident</small><br>' +
+			'<img class="legend-marker" src="https://api.tiles.mapbox.com/v3/marker/pin-s' 
+			+ '-' + icbcIcon.options.icon + '+' + icbcIcon.options.color + '.png"> <small>Cyclist incident insurance claim location</small>'
+
+			);
+		if(!DISABLE_GEOFENCES){
+			layerControl.addOverlay(alertAreas, "Alert Areas");
+		}
+		layerControl.addOverlay(racksCluster, "Bike Racks");
+		layerControl.addOverlay(heatMap, "Accident heat map");
+
+		layerControl.addTo(map);
 
 
-	/* ADD CUSTOM HELP BUTTON */ 
-	L.easyButton('bottomright', 'fa-question-circle',
-	         function(){toggleTooltips("show")},
-	         'Get Help'
-	);
+		/* GEOCODING SEARCH BAR CONTROL */
+		var geocoder = L.Control.geocoder({
+			position: "topright"
+		}).addTo(map);
+		
+		var geocodeMarker;
+		geocoder.markGeocode = function(result){
+			map.fitBounds(result.bbox);
+
+			if (geocodeMarker) {
+				map.removeLayer(geocodeMarker);
+			}
+
+			geocodeMarker = new L.Marker(result.center, {
+				icon: geocodeIcon
+			})
+				.bindPopup(result.name)
+				.addTo(map)
+				.openPopup();
+		};
+		
+
+		/* ADD SCALE BAR */
+		L.control.scale({
+			position: 'bottomright'
+		}).addTo(map);
 
 
-	/*ADD GPS BUTTON */
-	// map.addControl(new L.Control.Gps({
-	// 	// autoActive: true,
-	// 	title: 'Show your detected location',
-	// 	marker: new L.marker([0,0], {
-	// 	icon: locationIcon})
-	// }));
+		/* ADD CUSTOM HELP BUTTON */ 
+		L.easyButton('bottomright', 'fa-question-circle',
+		         function(){toggleTooltips("show")},
+		         'Get Help'
+		);
+
+
+		/*ADD GPS BUTTON */
+		// map.addControl(new L.Control.Gps({
+		// 	// autoActive: true,
+		// 	title: 'Show your detected location',
+		// 	marker: new L.marker([0,0], {
+		// 	icon: locationIcon})
+		// }));
+	};
 };
 
-/* GET GEOJSON STATIC LAYERS AND STORE AS LEAFLET FEATURE */
-function initializeGeoJsonLayers(){
-	var policePoints = new L.geoJson(policeData, {
-		pointToLayer: function(feature, latlng) {
-			heatMap.addLatLng(latlng);
 
-			return L.marker(latlng, {
-				icon: policeIcon
-			});
-		},
-		onEachFeature: function(feature, layer) {
-			var date = feature.properties.ACC_DATE.split("/");
-			date = getMonthFromInt(parseInt(date[1])) + ' ' + date[2] + ', ' + date[0]; 	// Month dd, YYYY
-			layer.bindPopup('<strong>Source:</strong> Victoria Police Dept.<br><strong>Date:</strong> ' + date);
-		}
-	}).addTo(accidentPoints),
+function setView(lat,lng,zoom) {
+	if(zoom){
+        this.map.setView(L.latLng(lat, lng), zoom);
+        locateUser(setView=false);
+	}
+    else {
+        locateUser(setView=true);
+    }
 
-	icbcPoints = new L.geoJson(icbcData, {
-		pointToLayer: function(feature, latlng) {
-			heatMap.addLatLng(latlng);
+	// Define event actions for finding the user's location
+	map.on('locationerror', onLocationError);
+	map.on('locationfound', onLocationFound);
+	
 
-			return L.marker(latlng, {
-				icon: icbcIcon
-			});
-		},
-		onEachFeature: function(feature, layer) {
-			var date = toTitleCase(feature.properties.Month) + " " + feature.properties.Year;
-			layer.bindPopup('<strong>Source:</strong> ICBC<br><strong>Date: </strong>' + date);
-		}
-	}).addTo(accidentPoints),
+	/* FIND AND RETURN THE USER'S LOCATION */
+	function locateUser(setView) {
+		this.map.locate({
+			setView: setView,
+			maxZoom: 16,
+			// watch: true,
+			enableHighAccuracy: true
+		});
+	};
 
-	bikeRacksVictoria = new L.geoJson(bikeRacks, {
-		pointToLayer: function(feature, latlng) {
-			return L.marker(latlng);
-		},
-		onEachFeature: function(feature, layer) {
-			layer.bindPopup('Bike rack');
-		}
-	}).addTo(racksCluster);
+
+	function onLocationError(e) {
+		alert(e.message);
+	};
+
+
+	function onLocationFound(e) {
+		// console.log('location found');
+	    // if(locationGroup) layerControl.removeLayer(locationGroup);
+	    var radius = e.accuracy / 2,
+
+		    marker = L.marker(e.latlng, {icon: locationIcon})
+		        .bindPopup("You are within " + radius + " meters of this point"),
+		    circle = L.circle(e.latlng, radius, {
+		      	color: "#" + locationIcon.options.color,
+				weight: 1,
+				opacity: 0.3,
+				clickable: false,
+				fillOpacity: 0.1
+		    });
+
+	    locationGroup = L.layerGroup([marker, circle]);
+	    layerControl.addOverlay(locationGroup, 'Detected location<br>' + 
+			'<img class="legend-marker" src="https://api.tiles.mapbox.com/v3/marker/pin-s' 
+				+ '-' + locationIcon.options.icon + '+' + locationIcon.options.color + '.png"> <small>You are here</small>');
+	    locationGroup.addTo(map);
+	};
 };
+
 
 // Purpose: Add a given latlng poing with the given information to the map. 
 // 		Add pk for easy lookup of marker for admin tasks
@@ -281,7 +296,7 @@ function getPoint(latlng, date, type, pk) {
 	}
 	marker = L.marker(latlng, {
 		icon: icon,
-		pk:pk,
+		pk: pk,
 		objType: 'point'
 	});
 
@@ -298,8 +313,8 @@ function getPolygon(latlng, pk) {
 		color: '#3b9972',
 		weight: 2,
 		opacity: 0.6,
-		fillOpacity:0.1,
-		pk: pk,	/*Mark the polygon with it's database id*/
+		fillOpacity: 0.1,
+		pk: pk, /*Mark the polygon with it's database id*/
 		objType: 'polygon'
 	}));
 };
@@ -371,108 +386,87 @@ function createPieCluster(cluster) {
 
 	// Build the svg layer
 	return pieChart(data, outerR, innerR, n);
+	
+
+	// pieChart
+	// 	Purpose: Builds the svg DivIcons
+	// 	inputs: data as list of objects containing "type", "count", "color", outer chart radius, inner chart radius, and total points for cluster
+	// 	output: L.DivIcon donut chart where each "type" is mapped to the corresponding "color" with a proportional section corresponding to "count"
+	function pieChart(data, outerR, innerR, total){
+		var arc = d3.svg.arc()
+			.outerRadius(outerR)
+			.innerRadius(innerR);
+
+		var pie = d3.layout.pie()
+			.sort(null)
+			.value(function(d) { return d.count; });
+
+		// Define the svg layer
+		var width = 50, 
+			height = 50;
+		var svg = document.createElementNS(d3.ns.prefix.svg, 'svg');	
+		var vis = d3.select(svg)
+			.data(data)
+			.attr('class', 'marker-cluster-pie')
+			.attr('width', width)
+			.attr('height', height)
+			.append("g")
+			.attr('transform', 'translate(' + width/2 + ',' + height/2 + ')');
+
+		var g = vis.selectAll(".arc")
+			.data(pie(data))
+			.enter().append("g")
+			.attr('class', 'arc');
+
+		g.append('path')
+			.attr("d", arc)
+			.style("fill", function(d) { return '#' + d.data.color; });
+
+		// Add center fill
+		vis.append("circle")
+			.attr("cx", 0)
+			.attr("cy", 0)
+			.attr("r", innerR)
+			.attr('class', 'center')
+			.attr("fill", "#f1f1f1");
+
+		// Add count text
+		vis.append('text')
+			.attr('class', 'pieLabel')
+			.attr('text-anchor', 'middle')
+			.attr('dy','.3em')
+			.text(total)
+
+		var html = serializeXmlNode(svg);
+
+		return new L.DivIcon({
+			html: html,
+			className: 'marker-cluster',
+			iconSize: new L.Point(40,40)
+		});
+		
+
+		// Purpose: Helper function to convert xmlNode to a string
+		function serializeXmlNode(xmlNode) {
+		    if (typeof window.XMLSerializer != "undefined") {
+		        return (new window.XMLSerializer()).serializeToString(xmlNode);
+		    } else if (typeof xmlNode.xml != "undefined") {
+		        return xmlNode.xml;
+		    }
+		    return "";
+		};
+	};
 };
 
 
-// pieChart
-// 	Purpose: Builds the svg DivIcons
-// 	inputs: data as list of objects containing "type", "count", "color", outer chart radius, inner chart radius, and total points for cluster
-// 	output: L.DivIcon donut chart where each "type" is mapped to the corresponding "color" with a proportional section corresponding to "count"
-function pieChart(data, outerR, innerR, total){
-	var arc = d3.svg.arc()
-		.outerRadius(outerR)
-		.innerRadius(innerR);
+function getMonthFromInt(num) {
+	months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+	return months[num - 1];
+};
 
-	var pie = d3.layout.pie()
-		.sort(null)
-		.value(function(d) { return d.count; });
 
-	// Define the svg layer
-	var width = 50, 
-		height = 50;
-	var svg = document.createElementNS(d3.ns.prefix.svg, 'svg');	
-	var vis = d3.select(svg)
-		.data(data)
-		.attr('class', 'marker-cluster-pie')
-		.attr('width', width)
-		.attr('height', height)
-		.append("g")
-		.attr('transform', 'translate(' + width/2 + ',' + height/2 + ')');
-
-	var g = vis.selectAll(".arc")
-		.data(pie(data))
-		.enter().append("g")
-		.attr('class', 'arc');
-
-	g.append('path')
-		.attr("d", arc)
-		.style("fill", function(d) { return '#' + d.data.color; });
-
-	// Add center fill
-	vis.append("circle")
-		.attr("cx", 0)
-		.attr("cy", 0)
-		.attr("r", innerR)
-		.attr('class', 'center')
-		.attr("fill", "#f1f1f1");
-
-	// Add count text
-	vis.append('text')
-		.attr('class', 'pieLabel')
-		.attr('text-anchor', 'middle')
-		.attr('dy','.3em')
-		.text(total)
-
-	var html = serializeXmlNode(svg);
-
-	return new L.DivIcon({
-		html: html,
-		className: 'marker-cluster',
-		iconSize: new L.Point(40,40)
+function toTitleCase(s) {
+	return s.replace(/\w\S*/g, function(txt) {
+		return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
 	});
-};
-
-// Purpose: Helper function to convert xmlNode to a string
-function serializeXmlNode(xmlNode) {
-    if (typeof window.XMLSerializer != "undefined") {
-        return (new window.XMLSerializer()).serializeToString(xmlNode);
-    } else if (typeof xmlNode.xml != "undefined") {
-        return xmlNode.xml;
-    }
-    return "";
-};
-
-
-function getMonthFromInt(num){
-	switch(num) {
-		case 1:
-			return "January";
-		case 2:
-			return "February";
-		case 3:
-			return "March";
-		case 4:
-			return "April";
-		case 5:
-			return "May";
-		case 6:
-			return "June";
-		case 7:
-			return "July";
-		case 8:
-			return "August";
-		case 9:
-			return "September";
-		case 10:
-			return "October";
-		case 11:
-			return "November";
-		case 12:
-			return "December";
-	}
-};
-
-
-function toTitleCase(s){
-    return s.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 };
