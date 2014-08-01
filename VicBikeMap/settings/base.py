1526ac95337859cacdd886b131bb874bfa24bde9
Taylor Denouden <tayden123@hotmail.com>
2014-08-01 16:42:18 -0700

    removed double google analytics code

--- VicBikeMap/settings/base.py
+++ VicBikeMap/settings/base.py
@@ -55,7 +55,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.static",
     "django.core.context_processors.tz",
     "django.contrib.messages.context_processors.messages",
-    "django.core.context_processors.request"
+    "django.core.context_processors.request",
 )
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
