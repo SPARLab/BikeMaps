3440729bde1ec73704f3c09e6e9a5f1426d10b4d
Taylor Denouden <tayden123@hotmail.com>
2014-08-01 13:02:51 -0700

    require_POST tag added to views to clean up logic flow

--- mapApp/views.py
+++ mapApp/views.py
@@ -5,6 +5,7 @@ from django.core.urlresolvers import reverse
 from django.core.exceptions import ValidationError
 
 from django.contrib.gis.geos import GEOSGeometry
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.core.mail import send_mail
 
@@ -67,150 +68,135 @@ def indexContext(request, incidentForm=IncidentForm(), routeForm=RouteForm(), ge
 def about(request):
 	return render(request, 'mapApp/about.html', {"emailForm": EmailForm()})
 
-
+@require_POST
 def contact(request):
-	if request.method == 'POST':
-		emailForm = EmailForm(request.POST)
+	emailForm = EmailForm(request.POST)
 
-		if emailForm.is_valid():
-			subject = emailForm.cleaned_data['subject']
-			message = emailForm.cleaned_data['message']
-			sender = emailForm.cleaned_data['sender']
-			cc_myself = emailForm.cleaned_data['cc_myself']
+	if emailForm.is_valid():
+		subject = emailForm.cleaned_data['subject']
+		message = emailForm.cleaned_data['message']
+		sender = emailForm.cleaned_data['sender']
+		cc_myself = emailForm.cleaned_data['cc_myself']
 
-			adminContacts = Group.objects.get(name="admin contact").user_set.all()
-			recipients = []
-			
-			for contact in adminContacts:
-				recipients.append(contact.email) 
-			if cc_myself:
-				recipients.append(sender)
+		adminContacts = Group.objects.get(name="admin contact").user_set.all()
+		recipients = []
+		
+		for contact in adminContacts:
+			recipients.append(contact.email) 
+		if cc_myself:
+			recipients.append(sender)
 
-			try:
-				send_mail(subject, message, sender, recipients)
-			except BadHeaderError:
-				messages.error(request, '<strong>Invalid Header.</strong> Illegal characters found.')
+		try:
+			send_mail(subject, message, sender, recipients)
+		except BadHeaderError:
+			messages.error(request, '<strong>Invalid Header.</strong> Illegal characters found.')
 
-			messages.success(request, '<strong>Thank you!</strong><br>We\'ll do our best to get back to you.')
-			emailForm = EmailForm() # Clear the form
+		messages.success(request, '<strong>Thank you!</strong><br>We\'ll do our best to get back to you.')
+		emailForm = EmailForm() # Clear the form
 
 	return render(request, 'mapApp/about.html', {"emailForm": emailForm})
 
-
+@require_POST
 def postRoute(request):
-	if request.method == 'POST':
-		routeForm = RouteForm(request.POST)
-		routeForm.data = routeForm.data.copy()
+	routeForm = RouteForm(request.POST)
+	routeForm.data = routeForm.data.copy()
 
-		# Convert string coords to valid geometry object
-		try:
-			routeForm.data['line'] = GEOSGeometry(routeForm.data['line'])
-		except(ValueError):
-			messages.error(request, '<strong>Error</strong><br>Invalid geometry data.')
-			return HttpResponseRedirect(reverse('mapApp:index')) 
+	# Convert string coords to valid geometry object
+	try:
+		routeForm.data['line'] = GEOSGeometry(routeForm.data['line'])
+	except(ValueError):
+		messages.error(request, '<strong>Error</strong><br>Invalid geometry data.')
+		return HttpResponseRedirect(reverse('mapApp:index')) 
 
-		if routeForm.is_valid():
-			routeForm.save()
-			routeForm = RouteForm() # Clear the form
-			messages.success(request, '<strong>Thank you!</strong><br>Your route was successfully added.')
+	if routeForm.is_valid():
+		routeForm.save()
+		routeForm = RouteForm() # Clear the form
+		messages.success(request, '<strong>Thank you!</strong><br>Your route was successfully added.')
 
 	return render(request, 'mapApp/index.html', indexContext(request, routeForm=routeForm))
 
-
+@require_POST
 def postIncident(request):
-	if request.method == 'POST':
-		incidentForm = IncidentForm(request.POST)
-		incidentForm.data = incidentForm.data.copy()
-		
-		# Convert coords to valid geometry
-		try:
-			incidentForm.data['geom'] = GEOSGeometry(incidentForm.data['geom'])
-		except(ValueError):
-			messages.error(request, '<strong>Error</strong><br>No point was selected for this type of report.')
-			return HttpResponseRedirect(reverse('mapApp:index')) 
-
-		if incidentForm.is_valid():
-			incident = incidentForm.save()
-			alertUsers(request, incident)
-			
-			messages.success(request, '<strong>Thank you!</strong><br>Your incident marker was successfully added.')			
-			return HttpResponseRedirect(reverse('mapApp:index', \
-				kwargs=({										\
-					"lat":str(incident.latlngList()[0]),		\
-					"lng":str(incident.latlngList()[1]),		\
-					"zoom":str(18)								\
-				})												\
-			)) 
-
-		else: # Show form errors 
-			return render(request, 'mapApp/index.html', indexContext(request, incidentForm=incidentForm))
+	incidentForm = IncidentForm(request.POST)
+	incidentForm.data = incidentForm.data.copy()
 	
-	else: # Redirect to index if not a post request
+	# Convert coords to valid geometry
+	try:
+		incidentForm.data['geom'] = GEOSGeometry(incidentForm.data['geom'])
+	except(ValueError):
+		messages.error(request, '<strong>Error</strong><br>No point was selected for this type of report.')
 		return HttpResponseRedirect(reverse('mapApp:index')) 
 
+	if incidentForm.is_valid():
+		incident = incidentForm.save()
+		alertUsers(request, incident)
 
+		messages.success(request, '<strong>Thank you!</strong><br>Your incident marker was successfully added.')			
+		return HttpResponseRedirect(reverse('mapApp:index', \
+			kwargs=({										\
+				"lat":str(incident.latlngList()[0]),		\
+				"lng":str(incident.latlngList()[1]),		\
+				"zoom":str(18)								\
+			})												\
+		)) 
+	else: # Show form errors 
+		return render(request, 'mapApp/index.html', indexContext(request, incidentForm=incidentForm))
+
+@require_POST
 def postHazard(request):
-	if request.method == 'POST':
-		hazardForm = HazardForm(request.POST)
-		hazardForm.data = hazardForm.data.copy()
-		
-		# Convert coords to valid geometry
-		try :
-			hazardForm.data['geom'] = GEOSGeometry(hazardForm.data['geom'])
-		except(ValueError):
-			messages.error(request, '<strong>Error</strong><br>No point was selected for this type of report.')
-			return HttpResponseRedirect(reverse('mapApp:index')) 
-
-		if hazardForm.is_valid():
-			hazard = hazardForm.save()
-			# alertUsers(request, hazard) #alertUsers view needs to be edited to accomodate hazards
-			
-			messages.success(request, '<strong>Thank you!</strong><br>Your hazard marker was successfully added.')			
-			return HttpResponseRedirect(reverse('mapApp:index', \
-				kwargs=({										\
-					"lat":str(hazard.latlngList()[0]),		\
-					"lng":str(hazard.latlngList()[1]),		\
-					"zoom":str(18)								\
-				})												\
-			)) 
-
-		else: # Show form errors 
-			return render(request, 'mapApp/index.html', indexContext(request, hazardForm=hazardForm))
+	hazardForm = HazardForm(request.POST)
+	hazardForm.data = hazardForm.data.copy()
 	
-	else: # Redirect to index if not a post request
+	# Convert coords to valid geometry
+	try :
+		hazardForm.data['geom'] = GEOSGeometry(hazardForm.data['geom'])
+	except(ValueError):
+		messages.error(request, '<strong>Error</strong><br>No point was selected for this type of report.')
 		return HttpResponseRedirect(reverse('mapApp:index')) 
 
+	if hazardForm.is_valid():
+		hazard = hazardForm.save()
+		# alertUsers(request, hazard) #alertUsers view needs to be edited to accomodate hazards
+		
+		messages.success(request, '<strong>Thank you!</strong><br>Your hazard marker was successfully added.')			
+		return HttpResponseRedirect(reverse('mapApp:index', \
+			kwargs=({										\
+				"lat":str(hazard.latlngList()[0]),		\
+				"lng":str(hazard.latlngList()[1]),		\
+				"zoom":str(18)								\
+			})												\
+		)) 
 
+	else: # Show form errors 
+		return render(request, 'mapApp/index.html', indexContext(request, hazardForm=hazardForm))
+
+@require_POST
 def postTheft(request):
-	if request.method == 'POST':
-		theftForm = TheftForm(request.POST)
-		theftForm.data = theftForm.data.copy()
-		
-		# Convert coords to valid geometry
-		try:
-			theftForm.data['geom'] = GEOSGeometry(theftForm.data['geom'])
-		except(ValueError):
-			messages.error(request, '<strong>Error</strong><br>No point was selected for this type of report.')
-			return HttpResponseRedirect(reverse('mapApp:index')) 
-
-		if theftForm.is_valid():
-			theft = theftForm.save()
-			# alertUsers(request, theft) #alertUsers view needs to be edited to accomodate thefts
-			
-			messages.success(request, '<strong>Thank you!</strong><br>Your theft marker was successfully added.')			
-			return HttpResponseRedirect(reverse('mapApp:index', \
-				kwargs=({										\
-					"lat":str(theft.latlngList()[0]),		\
-					"lng":str(theft.latlngList()[1]),		\
-					"zoom":str(18)								\
-				})												\
-			)) 
-
-		else: # Show form errors 
-			return render(request, 'mapApp/index.html', indexContext(request, theftForm=theftForm))
+	theftForm = TheftForm(request.POST)
+	theftForm.data = theftForm.data.copy()
 	
-	else: # Redirect to index if not a post request
+	# Convert coords to valid geometry
+	try:
+		theftForm.data['geom'] = GEOSGeometry(theftForm.data['geom'])
+	except(ValueError):
+		messages.error(request, '<strong>Error</strong><br>No point was selected for this type of report.')
 		return HttpResponseRedirect(reverse('mapApp:index')) 
+
+	if theftForm.is_valid():
+		theft = theftForm.save()
+		# alertUsers(request, theft) #alertUsers view needs to be edited to accomodate thefts
+		
+		messages.success(request, '<strong>Thank you!</strong><br>Your theft marker was successfully added.')			
+		return HttpResponseRedirect(reverse('mapApp:index', \
+			kwargs=({										\
+				"lat":str(theft.latlngList()[0]),		\
+				"lng":str(theft.latlngList()[1]),		\
+				"zoom":str(18)								\
+			})												\
+		)) 
+
+	else: # Show form errors 
+		return render(request, 'mapApp/index.html', indexContext(request, theftForm=theftForm))
 
 
 def alertUsers(request, incident):
@@ -233,25 +219,24 @@ def alertUsers(request, incident):
 	for user in usersToNotEmail:
 		AlertNotification(user=user, point=incident, action=action, emailed=True).save()
 
-
+@require_POST
 @login_required
 def postAlertPolygon(request):
-	if request.method == 'POST':
-		geofenceForm = GeofenceForm(request.POST)
-		geofenceForm.data = geofenceForm.data.copy()
+	geofenceForm = GeofenceForm(request.POST)
+	geofenceForm.data = geofenceForm.data.copy()
 
-		# Create valid attributes for user and geom fields
-		try:
-			geofenceForm.data['user'] = request.user.id
-			geofenceForm.data['geom'] = GEOSGeometry(geofenceForm.data['geom'])
-		except(ValueError):
-			messages.error(request, '<strong>Error</strong><br>Invalid geometry data.')
-			return HttpResponseRedirect(reverse('mapApp:index')) 
+	# Create valid attributes for user and geom fields
+	try:
+		geofenceForm.data['user'] = request.user.id
+		geofenceForm.data['geom'] = GEOSGeometry(geofenceForm.data['geom'])
+	except(ValueError):
+		messages.error(request, '<strong>Error</strong><br>Invalid geometry data.')
+		return HttpResponseRedirect(reverse('mapApp:index')) 
 
-		if geofenceForm.is_valid():
-			# Save new model object, send success message to the user
-			geofenceForm.save()
-			messages.success(request, 'You will now receive alerts for the area was traced.')
+	if geofenceForm.is_valid():
+		# Save new model object, send success message to the user
+		geofenceForm.save()
+		messages.success(request, 'You will now receive alerts for the area was traced.')
 
 	return HttpResponseRedirect(reverse('mapApp:index'))
 
@@ -297,39 +282,39 @@ def readAlertPoint(request, alertID):
 	else:
 		return HttpResponseRedirect(reverse('mapApp:index')) 
 
-
+@require_POST
 @login_required
 def editShape(request):
-	if(request.method == 'POST'):
-		editForm = EditForm(request.POST)
-		if editForm.is_valid():
+	editForm = EditForm(request.POST)
+	if editForm.is_valid():
 
-			pks = editForm.cleaned_data['editPk'].split(';')[:-1]
-			newGeoms = editForm.cleaned_data['editGeom'].split(';')[:-1]
-			editType = editForm.cleaned_data['editType']
-			objType = editForm.cleaned_data['objType']
+		pks = editForm.cleaned_data['editPk'].split(';')[:-1]
+		newGeoms = editForm.cleaned_data['editGeom'].split(';')[:-1]
+		editType = editForm.cleaned_data['editType']
+		objType = editForm.cleaned_data['objType']
 
-			if objType == 'point' and request.user.is_superuser:
-				objectSet = Incident.objects.all()
-			elif objType == 'polygon':
-				objectSet = AlertArea.objects.filter(user=request.user)
-			else:
-				return HttpResponseRedirect(reverse('mapApp:index'))
+		if objType == 'point' and request.user.is_superuser:
+			objectSet = Incident.objects.all()
+		elif objType == 'polygon':
+			objectSet = AlertArea.objects.filter(user=request.user)
+		else:
+			return HttpResponseRedirect(reverse('mapApp:index'))
 
-			for pk, newGeom in zip(pks, newGeoms):
-				if(editType == 'edit'):
-					shapeEdited = get_object_or_404(objectSet, pk=pk)
-					try:
-						shapeEdited.geom = GEOSGeometry(newGeom)	# edit the object geometry
-					except(ValueError):
-						messages.error(request, '<strong>Error</strong><br>Invalid geometry error.')
-						return HttpResponseRedirect(reverse('mapApp:index')) 
-					shapeEdited.save()
-			
-				elif (editType == 'delete'):
-					shapeEdited = get_object_or_404(objectSet, pk=pk)
-					shapeEdited.delete() 	# delete the object
+		for pk, newGeom in zip(pks, newGeoms):
+			if(editType == 'edit'):
+				shapeEdited = get_object_or_404(objectSet, pk=pk)
+				try:
+					shapeEdited.geom = GEOSGeometry(newGeom)	# edit the object geometry
+				except(ValueError):
+					messages.error(request, '<strong>Error</strong><br>Invalid geometry error.')
+					return HttpResponseRedirect(reverse('mapApp:index')) 
+				shapeEdited.save()
+		
+			elif (editType == 'delete'):
+				shapeEdited = get_object_or_404(objectSet, pk=pk)
+				shapeEdited.delete() 	# delete the object
 
-			message = str(len(pks)) + ' ' + editType + ('s' if len(pks)>1 else '') + ' successful'
-			messages.success(request, message)
+		message = str(len(pks)) + ' ' + editType + ('s' if len(pks)>1 else '') + ' successful'
+		messages.success(request, message)
+		
 	return HttpResponseRedirect(reverse('mapApp:index'))
