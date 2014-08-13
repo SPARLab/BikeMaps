07a35dab7943222503e41439b76e69ddea25187b
Taylor Denouden <tayden123@hotmail.com>
2014-08-13 14:58:31 -0700

    bug fix

--- mapApp/views.py
+++ mapApp/views.py
@@ -85,11 +85,11 @@ def contact(request):
 		
 		for contact in adminContacts:
 			recipients.append(contact.email) 
+		if cc_myself:
+			recipients.append(sender)
 
 		try:
 			send_mail(subject, message, sender, recipients)
-		if cc_myself:
-			recipients.append(sender)
 		except BadHeaderError:
 			messages.error(request, '<strong>Invalid Header.</strong> Illegal characters found.')
 
