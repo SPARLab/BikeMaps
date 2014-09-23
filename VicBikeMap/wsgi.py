e720bb11a22caddc0ac97a7bff74e369d5baab93
Taylor Denouden <tayden123@hotmail.com>
2014-09-23 11:01:43 -0700

    updated wsgi.url_scheme to https

--- VicBikeMap/wsgi.py
+++ VicBikeMap/wsgi.py
@@ -12,3 +12,5 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "VicBikeMap.settings.prod")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+os.environ['HTTPS'] = "on"
