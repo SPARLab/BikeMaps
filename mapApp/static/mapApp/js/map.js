3f994c07fab3faee9252e3f3bfe01c0b5d25522c
Taylor Denouden <tayden123@hotmail.com>
2015-02-12 10:52:41 -0800

    added details to the popups

--- mapApp/static/mapApp/js/map.js
+++ mapApp/static/mapApp/js/map.js
@@ -161,31 +161,33 @@ function loadGeojsonAjax(src, type){
 // Purpose: Initializes the Pie chart cluster icons by getting the needed attributes from each cluster
 //		and passing them to the pieChart function
 function serializeClusterData(cluster) {
-    var children = cluster.getAllChildMarkers(),
-        n = 0,
-        colorRef = {};
+  var children = cluster.getAllChildMarkers(),
+      n = 0,
+      colorRef = {};
 
-    for (var icon in icons) {
-        // Add a counting field to the icons objects
-        icons[icon]["count"] = 0;
-        // construct colorRef object for efficiency of bin sort
-        colorRef[icons[icon].options.color] = icon;
-    };
+  for (var icon in icons) {
+      // Add a counting field to the icons objects
+      icons[icon]["count"] = 0;
+      // construct colorRef object for efficiency of bin sort
+      colorRef[icons[icon].options.color] = icon;
+  };
 
-    //Count the number of markers in each cluster
-    children.forEach(function(child) {
-        // Match childColor to icon in icons
-        var icon = colorRef[child.options.icon.options.color];
-        icons[icon].count++;
-        n++;
-    });
+  //Count the number of markers in each cluster
+  children.forEach(function(child) {
+      // Match childColor to icon in icons
+      var icon = colorRef[child.options.icon.options.color];
+      icons[icon].count++;
+      n++;
+  });
 
-    // Make array of icons data
-    var data = $.map(icons, function(v) {
-      return v;
-    });
-    data.push(n);
-    return data;
+  // Make array of icons data
+  var data = $.map(icons, function(v) {
+    return v;
+  });
+  // Push total points in cluster
+  data.push(n);
+
+  return data;
 };
 
 
@@ -194,76 +196,77 @@ function serializeClusterData(cluster) {
 // 	inputs: data as list of objects containing "type", "count", "color", outer chart radius, inner chart radius, and total points for cluster
 // 	output: L.DivIcon donut chart where each "type" is mapped to the corresponding "color" with a proportional section corresponding to "count"
 function pieChart(data) {
-    var total = data.pop();
+  // Pop total points in cluster
+  var total = data.pop();
 
-    outerR = (total >= 10 ? (total < 50 ? 20 : 25) : 15),
-    innerR = (total >= 10 ? (total < 50 ? 10 : 13) : 7);
+  outerR = (total >= 10 ? (total < 50 ? 20 : 25) : 15),
+  innerR = (total >= 10 ? (total < 50 ? 10 : 13) : 7);
 
-    var arc = d3.svg.arc()
-        .outerRadius(outerR)
-        .innerRadius(innerR);
+  var arc = d3.svg.arc()
+      .outerRadius(outerR)
+      .innerRadius(innerR);
 
-    var pie = d3.layout.pie()
-        .sort(null)
-        .value(function(d) {
-            return d.count;
-        });
+  var pie = d3.layout.pie()
+      .sort(null)
+      .value(function(d) {
+          return d.count;
+      });
 
-    // Define the svg layer
-    var width = 50,
-        height = 50;
-    var svg = document.createElementNS(d3.ns.prefix.svg, 'svg');
-    var vis = d3.select(svg)
-        .data(data)
-        .attr('class', 'marker-cluster-pie')
-        .attr('width', width)
-        .attr('height', height)
-        .append("g")
-        .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
+  // Define the svg layer
+  var width = 50,
+      height = 50;
+  var svg = document.createElementNS(d3.ns.prefix.svg, 'svg');
+  var vis = d3.select(svg)
+      .data(data)
+      .attr('class', 'marker-cluster-pie')
+      .attr('width', width)
+      .attr('height', height)
+      .append("g")
+      .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
 
-    var g = vis.selectAll(".arc")
-        .data(pie(data))
-        .enter().append("g")
-        .attr('class', 'arc');
+  var g = vis.selectAll(".arc")
+      .data(pie(data))
+      .enter().append("g")
+      .attr('class', 'arc');
 
-    g.append('path')
-        .attr("d", arc)
-        .style("fill", function(d) {
-            return d.data.options.color;
-        });
+  g.append('path')
+      .attr("d", arc)
+      .style("fill", function(d) {
+          return d.data.options.color;
+      });
 
-    // Add center fill
-    vis.append("circle")
-        .attr("cx", 0)
-        .attr("cy", 0)
-        .attr("r", innerR)
-        .attr('class', 'center')
-        .attr("fill", "#f1f1f1");
+  // Add center fill
+  vis.append("circle")
+      .attr("cx", 0)
+      .attr("cy", 0)
+      .attr("r", innerR)
+      .attr('class', 'center')
+      .attr("fill", "#f1f1f1");
 
-    // Add count text
-    vis.append('text')
-        .attr('class', 'pieLabel')
-        .attr('text-anchor', 'middle')
-        .attr('dy', '.3em')
-        .text(total)
+  // Add count text
+  vis.append('text')
+      .attr('class', 'pieLabel')
+      .attr('text-anchor', 'middle')
+      .attr('dy', '.3em')
+      .text(total)
 
-    var html = serializeXmlNode(svg);
+  var html = serializeXmlNode(svg);
 
-    return new L.DivIcon({
-        html: html,
-        className: 'marker-cluster',
-        iconSize: new L.Point(40, 40)
-    });
+  return new L.DivIcon({
+      html: html,
+      className: 'marker-cluster',
+      iconSize: new L.Point(40, 40)
+  });
 
-    // Purpose: Helper function to convert xmlNode to a string
-    function serializeXmlNode(xmlNode) {
-        if (typeof window.XMLSerializer != "undefined") {
-            return (new window.XMLSerializer()).serializeToString(xmlNode);
-        } else if (typeof xmlNode.xml != "undefined") {
-            return xmlNode.xml;
-        }
-        return "";
-    };
+  // Purpose: Helper function to convert xmlNode to a string
+  function serializeXmlNode(xmlNode) {
+      if (typeof window.XMLSerializer != "undefined") {
+          return (new window.XMLSerializer()).serializeToString(xmlNode);
+      } else if (typeof xmlNode.xml != "undefined") {
+          return xmlNode.xml;
+      }
+      return "";
+  };
 };
 
 
@@ -292,14 +295,20 @@ function getPopup(layer) {
             popup += 'Due to';
         popup += ':</strong> ' + feature.properties.incident_with + '<br><strong>Date:</strong> ' + makeNiceDate(feature.properties.incident_date);
         if(feature.properties.incident_detail){
-          popup += '<br><strong>Details:</strong>' + feature.properties.incident_detail;
+          popup += '<br><strong>Details:</strong> <div class="popup-details">' + feature.properties.incident_detail + '</div>';
         }
 
     } else if (type === "hazard") {
         popup = '<strong>Hazard type:</strong> ' + feature.properties.hazard + '<br><strong>Date:</strong> ' + makeNiceDate(feature.properties.hazard_date);
+        if(feature.properties.hazard_detail){
+          popup += '<br><strong>Details:</strong> <div class="popup-details">' + feature.properties.hazard_detail + '</div>';
+        }
 
     } else if (type === "theft") {
         popup = '<strong>Theft type:</strong> ' + feature.properties.theft + '<br><strong>Date:</strong> ' + makeNiceDate(feature.properties.theft_date);
+        if(feature.properties.theft_detail){
+          popup += '<br><strong>Details:</strong> <div class="popup-details">' + feature.properties.theft_detail + '</div>';
+        }
 
     } else if (type === "icbc") {
         var date = toTitleCase(feature.properties.Month).substring(0, 3) + ". " + feature.properties.Year;
