a03d0752c5310de3c789cfb86fa13010536cb965
Taylor Denouden <tayden123@hotmail.com>
2015-02-16 12:43:26 -0800

    stats page workign

--- mapApp/views/statistics.py
+++ mapApp/views/statistics.py
@@ -12,10 +12,8 @@ def stats(request):
 	user = request.user
 
 	# Get the user's alertable points in the last month
-	incidents = Incident.objects.all()#filter(incidentNotification__user=user.id)
-	nearmisses = incidents.filter(incident__contains="Near collision")
-	collisions = incidents.exclude(incident__contains="Near collision")
-
+	collisions = Incident.objects.filter(p_type__exact="collision") | Incident.objects.filter(p_type__exact="fall")
+	nearmisses = Incident.objects.filter(p_type__exact="nearmiss")
 	hazards = Hazard.objects.all()
 	thefts = Theft.objects.all()
 
@@ -38,7 +36,6 @@ def stats(request):
 
 		'geofences': rois,
 
-		'incidents': incidents,
 		'collisions': collisions,
 		'nearmisses': nearmisses,
 		'hazards': hazards,
@@ -48,11 +45,6 @@ def stats(request):
 		'nearmissesInPoly': nearmissesInPoly,
 		'hazardsInPoly': hazardsInPoly,
 		'theftsInPoly': theftsInPoly,
-
-		# 'collisionsOutPoly': collisions.exclude(pk__in=collisionsInPoly),
-		# 'nearmissesOutPoly': nearmisses.exclude(pk__in=nearmissesInPoly),
-		# 'hazardsOutPoly': hazards.exclude(pk__in=hazardsInPoly),
-		# 'theftsOutPoly': thefts.exclude(pk__in=theftsInPoly),
 	}
 
 	return render(request, 'mapApp/stats.html', context)
@@ -62,11 +54,8 @@ def stats(request):
 def experimental(request):
 	user = request.user
 
-	# Get the user's alertable points in the last month
-	incidents = Incident.objects.all()#filter(incidentNotification__user=user.id)
-	nearmisses = incidents.filter(incident__contains="Near collision")
-	collisions = incidents.exclude(incident__contains="Near collision")
-
+	collisions = Incident.objects.filter(p_type__exact="collision") | Incident.objects.filter(p_type__exact="fall")
+	nearmisses = Incident.objects.filter(p_type__exact="nearmiss")
 	hazards = Hazard.objects.all()
 	thefts = Theft.objects.all()
 
