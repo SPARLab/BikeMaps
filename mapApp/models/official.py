ea7d6656636abaa49d111123e61978f84e7bd825
Taylor Denouden <tayden123@hotmail.com>
2015-02-27 12:10:07 -0800

    added geometry field to fixtures, removed Point model inheritance in Official Model

--- mapApp/models/official.py
+++ mapApp/models/official.py
@@ -2,13 +2,35 @@ from django.conf import settings
 from django.contrib.gis.db import models
 from point import Point
 
+import datetime
+from time import strftime, gmtime
+from django.utils import timezone
+
 ##########
 # Official class.
 # Class for storing official data. Contains fields that official data should be modified to fit as best as possible.
-class Official(Point):
-    point = models.OneToOneField(Point, parent_link=True)
+class Official(models.Model):
+    geom = models.PointField('Location')
+    objects = models.GeoManager() # Required to conduct geographic queries
 
+    report_date = models.DateTimeField(
+        auto_now_add=True   # Date is set automatically when object created
+    )
+    date = models.DateField(
+        blank=True,
+        null=True
+    )
+    time = models.TimeField(
+        blank=True,
+        null=True
+    )
+
+    p_type = models.CharField(
+        default="official",
+        max_length=150,
+    )
     official_type = models.CharField(
+        default="Vehicle collision",
         max_length=200,
     )
     data_source = models.CharField(
@@ -17,6 +39,8 @@ class Official(Point):
     metadata = models.CharField(
         max_length=500,
     )
-    who_added = models.CharField(
-        max_length=100,
+    details = models.TextField(
+        max_length=300,
+        blank=True,
+        null=True
     )
