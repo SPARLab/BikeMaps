1623d8ee8945572cbaa63ba206d738b724e6b60e
Taylor Denouden <tayden123@hotmail.com>
2015-05-26 15:48:46 -0700

    Delete troublesome admin-only tooltip

--- mapApp/static/mapApp/js/tooltips.js
+++ mapApp/static/mapApp/js/tooltips.js
@@ -3,8 +3,6 @@ $(function(){
 	$('.leaflet-control-geocoder').tooltip({placement:'right', delay:400, title: function(){ return "Search for an address"}});
 
 	map.on('drawControlReady', function(){
-		// delete points control
-	 	$('.leaflet-right a.leaflet-draw-edit-remove').tooltip({placement:'left', delay: 400, title: function(){return 'Delete user points'}});
 		// All left side controls
 		$('.leaflet-left .leaflet-control a').tooltip({placement:'right', delay: 400});
 	});
