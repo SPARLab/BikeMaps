db18414d0bd2cab5ab79c3cbc2dbd4dec731a673
Taylor Denouden <tayden123@hotmail.com>
2015-05-26 15:46:47 -0700

    Fix map tooltips

--- mapApp/static/mapApp/js/tooltips.js
+++ mapApp/static/mapApp/js/tooltips.js
@@ -1,8 +1,11 @@
-$(document).ready(function(){
+$(function(){
 	// search control
 	$('.leaflet-control-geocoder').tooltip({placement:'right', delay:400, title: function(){ return "Search for an address"}});
- 	// delete points control
- 	$('.leaflet-right .leaflet-draw-edit-remove').tooltip({placement:'left', delay: 400, title: function(){return 'Delete user points'}});
-	// All left side controls
-	$('.leaflet-left .leaflet-control a').tooltip({placement:'right', delay: 400});
+
+	map.on('drawControlReady', function(){
+		// delete points control
+	 	$('.leaflet-right a.leaflet-draw-edit-remove').tooltip({placement:'left', delay: 400, title: function(){return 'Delete user points'}});
+		// All left side controls
+		$('.leaflet-left .leaflet-control a').tooltip({placement:'right', delay: 400});
+	});
 });
