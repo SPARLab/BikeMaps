b5ed1b6e9c55faa935be354b9ad9fb0ca98cd5f1
dgboss <darrengboss@gmail.com>
2015-05-27 11:06:38 -0700

    Typo!

--- mapApp/views/restApi.py
+++ mapApp/views/restApi.py
@@ -141,7 +141,7 @@ class OfficialList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    """"
+    """
 
 
 class AlertAreaList(APIView):
