0c3234b89201a393bef26d35f8bf28ac705789d1
Taylor Denouden <tayden123@hotmail.com>
2015-06-04 16:14:32 -0700

    Add documentation strings to hash62 functions

--- blogApp/utils/hash62.py
+++ blogApp/utils/hash62.py
@@ -1,6 +1,7 @@
 ALPHA62 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 def hash(i10):
+    """ Convert a base-10 integer into a base-62 integer and return the string """
     digits = []
     while i10 > 0:
         digits.append(i10%62)
@@ -8,6 +9,7 @@ def hash(i10):
     return "".join(map(lambda x: ALPHA62[x], digits))
 
 def dehash(s62):
+    """ Convert a base-62 integer (as type:string) into a base-10 integer and return the integer """
     res = 0
     for i, c in enumerate(s62):
         res += ALPHA62.index(c) * 62**i
