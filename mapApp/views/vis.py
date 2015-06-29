64a482c595a9a576b6fc844b6e919c17f73ef844
Taylor Denouden <tayden123@hotmail.com>
2015-06-29 16:25:37 -0700

    Define coordinates-based url for vis page and auto update it on panning of the vis map

--- mapApp/views/vis.py
+++ mapApp/views/vis.py
@@ -2,9 +2,16 @@ from django.shortcuts import render
 
 from mapApp.models import AlertArea, Point
 
-def vis(request):
+def vis(request, lat=None, lng=None, zoom=None):
 	context = {
 		'alertAreas': AlertArea.objects.filter(user=request.user.id),
 		'points': Point.objects.all()
 	}
+	
+	# Add zoom and center data if present
+	if not None in [lat, lng, zoom]:
+		context['lat']= float(lat)
+		context['lng']= float(lng)
+		context['zoom']= int(zoom)
+
 	return render(request, 'mapApp/vis.html', context)
