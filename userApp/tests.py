b9a50201bd86c07a7215ad1e1304545a5fc44ccc
Taylor Denouden <taylordenouden@gmail.com>
2015-08-06 18:09:48 -0700

    Implement some tests for userApp

--- userApp/tests.py
+++ userApp/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+class BasicTestCase(TestCase):
+    """Test getting various urls for user app"""
+    def test_getting_login(self):
+        self.client.get('/user/login')
+
+    def test_getting_register(self):
+        self.client.get('/user/register')
+
+    def test_getting_(self):
+        self.client.get('/alerts')
