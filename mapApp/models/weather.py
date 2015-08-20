82c0d7c9a779b9165969593a59400218e5a952ab
Taylor Denouden <taylordenouden@gmail.com>
2015-08-20 16:27:28 -0700

    Improve weather form labels in admin

--- mapApp/models/weather.py
+++ mapApp/models/weather.py
@@ -6,20 +6,24 @@ class Weather(models.Model):
 
     incident = models.OneToOneField(Incident, primary_key=True)
 
-    temperature_c = models.FloatField()
-    visibility_km = models.FloatField()
-    windspeed_kmh = models.FloatField()
-    precip_mmh = models.FloatField()
-    precip_prob = models.FloatField()
-    sunrise_time = models.DateTimeField()
-    sunset_time = models.DateTimeField()
-    dawn = models.BooleanField()
-    dusk = models.BooleanField()
-    wind_dir_deg = models.FloatField()
-    wind_dir_str = models.CharField(max_length=5)
-    black_ice_risk = models.BooleanField()
-    summary = models.CharField(max_length=250)
+    temperature_c = models.FloatField("Temperature (C)")
+    visibility_km = models.FloatField("Visibility (km)")
+    windspeed_kmh = models.FloatField("Wind speed (km/h)")
+    precip_mmh = models.FloatField("Precipitation intensity (mm/h)")
+    precip_prob = models.FloatField("Precipitation probability")
+    sunrise_time = models.DateTimeField("Sunrise time")
+    sunset_time = models.DateTimeField("Sunset time")
+    dawn = models.BooleanField("The accident occurred at dawn")
+    dusk = models.BooleanField("The accident occurred at dusk")
+    wind_dir_deg = models.FloatField("Wind origin (deg)")
+    wind_dir_str = models.CharField("Wind origin", max_length=5)
+    black_ice_risk = models.BooleanField("Black ice risk present")
+    summary = models.CharField("Summary", max_length=250)
 
     # toString()
     def __unicode__(self):
         return unicode(self.summary)
+
+    class Meta:
+        verbose_name = "Weather"
+        verbose_name_plural = "Weather"
