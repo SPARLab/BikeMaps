c8846109ac48e3db4b7929b87bb8e6772cdf5b9d
Sandeep Sabu <ssabu3@asurite.asu.edu>
2020-04-22 14:18:30 -0700

    spanish translation finished-form,disclaimer,etc-migrations from mar 13 backup

--- VicBikeMap/settings/dev.py
+++ VicBikeMap/settings/dev.py
@@ -10,6 +10,14 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 from VicBikeMap.settings.base import *
 import os
+if os.name == 'nt':
+    import platform
+    OSGEO4W = r"C:\OSGeo4W"
+    assert os.path.isdir(OSGEO4W), "Directory does not exist: " + OSGEO4W
+    os.environ['OSGEO4W_ROOT'] = OSGEO4W
+    os.environ['GDAL_DATA'] = OSGEO4W + r"\share\gdal"
+    os.environ['PROJ_LIB'] = OSGEO4W + r"\share\proj"
+    os.environ['PATH'] = OSGEO4W + r"\bin;" + os.environ['PATH']
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '$e05l@n&nv*zh@m=4dgx8j-rj^$w2ugj%$&*99=p$vwd5%ya53'
@@ -24,9 +32,9 @@ DATABASES = {
     'default': {
         # PostgreSQL database connection on Taylor's Windows computer
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'bikeDB',
+        'NAME': 'bikemap_db',
         'USER': 'postgres',
-        'PASSWORD': ''
+        'PASSWORD': 'Spi9dlee6'
     }
 }
 
