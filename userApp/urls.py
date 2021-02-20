8a573509dbbf20963b1804b4bd2fcd4bdf91391a
Zahin430 <zhprangon@mun.ca>
2021-02-20 18:44:47 -0330

    fix logoutview bug

--- userApp/urls.py
+++ userApp/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^profile/$', views.profile, name='profile'),
     url(r'^login/$', views.rate_limit_login, name='login'),
     url(r'^rate_limited/$', views.rate_limited, name='rate_limited'),
-    url(r'^logout/$', django_contrib_auth_views.LogoutView, {'template_name': 'userApp/logged_out.html', 'next_page': '/'}, name='logout'),
+    url(r'^logout/$', django_contrib_auth_views.LogoutView.as_view( template_name='userApp/logged_out.html', next_page= '/' ), name='logout'),
     url(r'^password_change/$', django_contrib_auth_views.PasswordChangeView, {'template_name': 'userApp/password_change_form.html', 'post_change_redirect': reverse_lazy('userApp:password_change_done')}, name='password_change'),
     url(r'^password_change/done/$', django_contrib_auth_views.PasswordChangeDoneView, {'template_name': 'userApp/password_change_done.html'}, name='password_change_done'),
     url(r'^password_reset/$', django_contrib_auth_views.PasswordResetView, {'template_name': 'userApp/password_reset_form.html', 'email_template_name': 'userApp/password_reset_email.html', 'post_reset_redirect': reverse_lazy('userApp:password_reset_done')}, name='password_reset'),
