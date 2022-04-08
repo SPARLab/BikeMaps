859b80af8726445c26622383e4cc3f2da57df873
Finn <finn.d.short@gmail.com>
2022-04-08 11:39:36 -0400

    Clean up and document map initialization script

--- mapApp/static/mapApp/js/index.js
+++ mapApp/static/mapApp/js/index.js
@@ -1,79 +1,9 @@
-function trySettingLocationFromURL() {
-  let locationNotSetInURL = (typeof lat === 'undefined' || typeof lng === 'undefined' || typeof zoom === 'undefined');
-  if (!locationNotSetInURL) {
-    console.log(`going to url location`);
-    mapCenter = [lat, lng];
-    mapZoom = zoom;
-    return true;
-  }
-  return false;
-}
-
-async function checkLocationPermissions() {
-  if (navigator.geolocation) {
-    const result = await navigator.permissions.query({
-      name: 'geolocation'
-    });
-    if (result.state === 'granted') {
-      console.log("returning true")
-      return true;
-    } else {
-      console.log('Permissions either denied or not requested yet');
-      return false;
-    }
-  }
-  console.log("Geolocation is not supported by this browser.")
-  return false;
-}
-
-function getUserLocation() {
-  return new Promise(function(resolve, reject) {
-    navigator.geolocation.getCurrentPosition(
-      pos => {
-        resolve(pos);
-      },
-      err => {
-        reject(err);
-      }, {
-        timeout: 5000
-      }
-    );
-  });
-}
-
-function trySettingLocationFromStorage() {
-  let lastKnownLat = localStorage.getItem('lastKnownLat');
-  let lastKnownLng = localStorage.getItem('lastKnownLng');
-  let lastKnownZoom = localStorage.getItem('lastKnownZoom');
-
-  let haveLastKnownLocation = (lastKnownLat && lastKnownLng && lastKnownZoom);
-
-  // 3. If last known lat, long, zoom are available, go there
-  if (haveLastKnownLocation) {
-    console.log(`going to last known location`);
-    mapCenter = [lastKnownLat, lastKnownLng];
-    mapZoom = lastKnownZoom;
-    window.history.replaceState({}, "", "@" + Number(lastKnownLat).toFixed(7) + "," + Number(lastKnownLng).toFixed(7) + "," + Number(lastKnownZoom) + "z");
-  }
-  // 4. As a last resort, load the default location
-  else setDefaultLocation();
-}
-
-function setDefaultLocation() {
-  console.log(`going to default location`);
-  mapCenter = [48, -100];
-  mapZoom = 4;
-}
-
-const mapInitializedEvent = new Event('mapInitializedEvent', {
-  bubbles: true,
-});
+/* This is the entry point into the script that runs on the main page of BikeMaps. The inital location for the map is determined before any of the map tiles are rendered or incident data is retreived. Any code that requires the map to be initalized and location set runs in this block, while other variable and function definitions can be found in 'index-helpers.js'
+ */
 
 initializeMapLocation().then(() => {
-  console.log('got initial location');
-  console.log(mapCenter);
-  console.log(mapZoom);
 
+  /** Initalize map */
   map = L.map('map', {
     center: mapCenter,
     minZoom: 2,
@@ -83,15 +13,35 @@ initializeMapLocation().then(() => {
     worldCopyJump: true,
   });
 
-  // Trigger an event to let 'draw.html' know the map is initalized and the draw functions can be added. Only run once document is ready to make sure listener has been added.
+  /** The drawing functionality is stored in 'draw.html', which allows it to access Django template tags. This custom event fires to let draw.html know the map has been initialized and the drawing code can run. It runs document is ready to make sure listener in draw.html has been added.  */
+  const mapInitializedEvent = new Event('mapInitializedEvent', {
+    bubbles: true,
+  });
+
   $(document).ready(function() {
     document.dispatchEvent(mapInitializedEvent);
   });
 
-  // Initial data load
-  loadAllIncidentData(map.getBounds());
+  /** Style map control tooltips once draw control has loaded */
+  map.on('drawControlReady', function() {
+    // All left side controls
+    $('.leaflet-left .leaflet-control a').tooltip({
+      placement: 'right',
+      delay: 400
+    });
+  });
 
-  /** Add geocoder control */
+  /** Load the data for the initial map bounds and add layer to map */
+  loadAllIncidentData(map.getBounds());
+  incidentAppliedLayers.addTo(map);
+
+  /** Define data fetch function for popup content when marker is clicked */
+  incidentAppliedLayers.on('click', function(e) {
+    var layer = e.layer;
+    getXHRPopup(layer);
+  });
+
+  /** Add geocoder control to toolbar */
   var geocodeMarker;
   var geocoder = L.Control.geocoder({
     defaultMarkGeocode: false,
@@ -100,7 +50,8 @@ initializeMapLocation().then(() => {
     errorMessage: gettext('Nothing found.')
   }).on('markgeocode', function(result) {
     map.fitBounds(result.geocode.bbox);
-    geocodeMarker && map.removeLayer(geocodeMarker); //remove old marker if it exists
+    // remove old marker if it exists
+    geocodeMarker && map.removeLayer(geocodeMarker);
     geocodeMarker = new L.Marker(result.geocode.center, {
       icon: icons["geocodeIcon"]
     }).bindPopup(result.geocode.name).addTo(map).openPopup();
@@ -114,11 +65,7 @@ initializeMapLocation().then(() => {
     }
   });
 
-  // If lat/long/zoom have been passed into the URL, set view to that location
-  // Locate the user either way, but if view was already set don't change view to users location
-
-  /** TEMPORARILY TURN OFF GEOLOCATION WHILE WORKING ON LOCAL STORAGE **/
-  // locateUser(setView = locationNotSetInURL, watch = false);
+  /** Add user location control to toolbar */
   const lc = L.control.locate({
     strings: {
       title: "Go to my location"
@@ -128,7 +75,7 @@ initializeMapLocation().then(() => {
     }
   }).addTo(map);
 
-  /** Add zoom control */
+  /** Add zoom control to toolbar */
   const zoomControl = L.control.zoom({
     zoomInTitle: gettext('Zoom in'),
     zoomOutTitle: gettext('Zoom out'),
@@ -147,7 +94,7 @@ initializeMapLocation().then(() => {
   L.DomEvent.on(elem, 'mouseover', () => map.dragging.disable());
   L.DomEvent.on(elem, 'mouseleave', () => map.dragging.enable());
 
-
+  /** When user has finished panning or jumping to a new location, the map center and zoom level are updated in the URL. They are also saved to local storage, which can be used to set the initial map location if the user revisits the site */
   map.on('moveend', function(e) {
     let center = map.getCenter();
     lastKnownZoom = map.getZoom();
@@ -163,62 +110,88 @@ initializeMapLocation().then(() => {
     }
   });
 
-  // Style map control tooltips
-  map.on('drawControlReady', function() {
-    // All left side controls
-    $('.leaflet-left .leaflet-control a').tooltip({
-      placement: 'right',
-      delay: 400
-    });
-  });
-
-  // /** Locate user, set map's view */
-  //
-  // // Find the user via GPS or internet connection.
-  // // Parameters to determine if the maps view should be set to that location and if the position should be polled and updated
-  // // If successful, fires a 'locationfound' event
-  // function locateUser(setView, watch) {
-  //     this.map.locate({
-  //         setView: setView,
-  //         maxZoom: 16,
-  //         watch: watch,
-  //         enableHighAccuracy: true
-  //     });
-  // };
-  //
-  // // If the users location is found, set map view to that location
-  // map.on("locationfound", function (location) {
-  //
-  // });
-
-  incidentAppliedLayers.addTo(map);
-
-
-  // Loads data for map view that was loaded when site first visited
-  // Sometimes site goes to default location and then receives 'locationfound' event and switches to users location right away, causing an apparent double loading. Don't want to wait for location found to load data so don't think this is a bug, but maybe a better way of doing it?
-  // if (!loadingDataFlag) loadAllIncidentData(map.getBounds());
-
-  // Define popup getter function
-  incidentAppliedLayers.on('click', function(e) {
-    var layer = e.layer;
-    getXHRPopup(layer);
-  });
-
-  /**
-   * Alert areas
-   */
-
+  /** Add alert areas and geofences to map */
   alertAreas.addTo(map);
-  // Add geofence alert areas to map
   addAlertAreas(geofences);
+});
 
-})
+/** Check if the URL contains lat/long/zoom that can be used to initalize map location */
+function trySettingLocationFromURL() {
+  let locationNotSetInURL = (typeof lat === 'undefined' || typeof lng === 'undefined' || typeof zoom === 'undefined');
+  // Set map center and map zoom if available in URL
+  if (!locationNotSetInURL) {
+    mapCenter = [lat, lng];
+    mapZoom = zoom;
+    return true;
+  }
+  return false;
+}
 
+/** This functions checks if the user has granted location permissions without requesting them. This aligns with Google's recommendation to only 'request geolocation information in response to a user gesture' rather than on page load, which can make users mistrustful or confused. */
+async function checkLocationPermissions() {
+  if (navigator.geolocation) {
+    const result = await navigator.permissions.query({
+      name: 'geolocation'
+    });
+    if (result.state === 'granted') {
+      // Location permissions have already been granted
+      return true;
+    } else {
+      // Permissions either denied or not requested yet
+      return false;
+    }
+  }
+  // Geolocation is not supported by this browser
+  return false;
+}
+
+/** This function fetches users location */
+function getUserLocation() {
+  return new Promise(function(resolve, reject) {
+    navigator.geolocation.getCurrentPosition(
+      pos => {
+        resolve(pos);
+      },
+      err => {
+        reject(err);
+      }, {
+        timeout: 5000
+      }
+    );
+  });
+}
+
+//* This function checks if we have the location that the user last visited in local storage, and uses it if available */
+function trySettingLocationFromStorage() {
+  let lastKnownLat = localStorage.getItem('lastKnownLat');
+  let lastKnownLng = localStorage.getItem('lastKnownLng');
+  let lastKnownZoom = localStorage.getItem('lastKnownZoom');
+
+  let haveLastKnownLocation = (lastKnownLat && lastKnownLng && lastKnownZoom);
+
+  if (haveLastKnownLocation) {
+    mapCenter = [lastKnownLat, lastKnownLng];
+    mapZoom = lastKnownZoom;
+    window.history.replaceState({}, "", "@" + Number(lastKnownLat).toFixed(7) + "," + Number(lastKnownLng).toFixed(7) + "," + Number(lastKnownZoom) + "z");
+  }
+  // If every other map initalization option has failed, load the default location
+  else setDefaultLocation();
+}
+
+/** This function sets the location to a zoomed-out view of North America */
+function setDefaultLocation() {
+  mapCenter = [48, -100];
+  mapZoom = 4;
+}
+
+/** This function determines where the map location will be initialized to when the front page loads. The order of priority is:
+1. The location and zoom level set in the url (eg bikemaps.org/@48.5,-123.5,13z)
+2. The user's current location, IF location permissions have previously been granted
+3. The last location visited by the user if available
+4. A view of North America
+*/
 async function initializeMapLocation() {
-  /** Initialize the map location */
-  /** lat, lng, zoom are retreived from URL and will be undefined if not set. Last known lat, long, zoom are from local storage and will be null if not set. */
 
-  // 1. If location is in URL, go there
   const locationInURL = trySettingLocationFromURL();
 
   if (!locationInURL) {
@@ -227,34 +200,25 @@ async function initializeMapLocation() {
       if (haveLocationPermission) {
         try {
           const position = await getUserLocation();
-          console.log("position");
-          console.log(position);
 
           if (position) {
-            // SUCCESSFULLY GOT USER LOCATION
-            console.log("successfully got the position: ");
-            console.log(position);
+            // Successfully retreived user's location, set map view
             mapCenter = [position.coords.latitude, position.coords.longitude];
             mapZoom = 16;
           } else {
             // Did not successfully retreive users location
-            console.log(`Did not successfully retreive users location`);
             trySettingLocationFromStorage();
           }
         } catch (error) {
           // We have location permissions but there was an error getting the user's location
-          console.log(`We have location permissions but there was an error getting the user's location`)
-          console.error(error);
           trySettingLocationFromStorage();
         }
       } else {
         // We don't have location permissions
-        console.log(`We don't have location permissions`);
         trySettingLocationFromStorage();
       }
     } catch (error) {
       // There was an error when trying to get location permissions
-      console.error(error);
       trySettingLocationFromStorage();
     }
   }
