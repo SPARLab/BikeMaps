c7ce5845517889c8ed052395bf348f5659c2f8d0
Finn <finn.d.short@gmail.com>
2022-05-30 18:14:49 -0400

    remove esri layer

--- mapApp/static/mapApp/js/map.js
+++ mapApp/static/mapApp/js/map.js
@@ -19,7 +19,6 @@ var MapQuestOpen_OSM = L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/map/{z
         maxZoom: 14,
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }),
-    Esri_Streets_Basemap = L.esri.basemapLayer("Streets"),
     Mapnik_BW = L.tileLayer('http://{s}.www.toolserver.org/tiles/bw-mapnik/{z}/{x}/{y}.png', {
       attribution: '<a href="https://openstreetmap.org/">&copy OpenStreetMap contributors, CC-BY-SA</a>',
       subdomains: '1234'
