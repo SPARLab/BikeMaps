0b174cdc73c36373af933f65c7e8bcf6c118dc1f
Finn <finn.d.short@gmail.com>
2022-06-19 17:31:44 -0400

    Add local storage location to vis page

--- mapApp/static/mapApp/js/vis/map.js
+++ mapApp/static/mapApp/js/vis/map.js
@@ -18,8 +18,18 @@ var map = L.map('map', {
     zoomOutTitle: gettext('Zoom out'),
   }).addTo(map);
 
+/* If coordinates have been passed in through the URL, go to that location
+** If not, go to the last known location for a smooth transition between
+** homepage and vis page
+** Default view is global (zoom of 1)
+*/
 if (typeof zoom !== 'undefined') {
   map.setView(L.latLng(lat, lng), zoom);
+} else if (localStorage.getItem('lastKnownLat') !== null) {
+  let lastKnownLat = localStorage.getItem('lastKnownLat');
+  let lastKnownLng = localStorage.getItem('lastKnownLng');
+  let lastKnownZoom = localStorage.getItem('lastKnownZoom');
+  map.setView(L.latLng(lastKnownLat, lastKnownLng), lastKnownZoom);
 }
 
 /** Add geocoder control */
