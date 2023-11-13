7ba08c43073f1cb6e7e50e71795aa746c2cd6f49
danwill <daniel.willett2@gmail.com>
2023-11-13 10:50:40 -0800

    removed email edits from base.py

--- VicBikeMap/settings/base.py
+++ VicBikeMap/settings/base.py
@@ -39,9 +39,6 @@ AUTH_USER_MODEL = 'spirit.User'
 LOGIN_URL = 'userApp:login'
 LOGIN_REDIRECT_URL = 'mapApp:index'
 
-EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
-EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
-
 POSTGIS_VERSION = (2, 1, 3)
 
 # Internationalization
@@ -220,4 +217,3 @@ CORS_ALLOW_HEADERS = (
     'authorization',
     'X-CSRFToken'
 )
-
